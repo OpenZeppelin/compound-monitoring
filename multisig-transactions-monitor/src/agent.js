dda22bf6e308640f4bf5bc7179045f43c64405eb
Darian <darian@arbitraryexecution.com>
2022-04-04 10:30:10 -0700

    initial agent setup

---
+++ multisig-transactions-monitor/src/agent.js
@@ -0,0 +1,11 @@
+const { Finding, FindingSeverity, FindingType } = require("forta-agent");
+
+const handleTransaction = async (txEvent) => {
+  const findings = [];
+
+  return findings
+};
+
+module.exports = {
+  handleTransaction
+};
