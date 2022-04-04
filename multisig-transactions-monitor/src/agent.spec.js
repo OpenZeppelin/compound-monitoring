fb4724f3e2453391bdba300967b8a58052b2b32f
Darian <darian@arbitraryexecution.com>
2022-04-04 14:37:54 -0700

    created a finding if multisig address is involed in a transaction

--- multisig-transactions-monitor/src/agent.spec.js
+++ multisig-transactions-monitor/src/agent.spec.js
@@ -4,9 +4,51 @@ const {
   Finding,
   createTransactionEvent,
   ethers,
-} = require("forta-agent");
-const {
-  handleTransaction,
-} = require("./agent");
+} = require('forta-agent');
 
-describe("Compound Finance multisig transaction agent", () => {});
+// require the agent
+const { provideHandleTransaction, provideInitialize,} = require('./agent');
+
+const config = require('../agent-config.json');
+
+// check config file
+describe('check agent configuration file', () => {
+  it('has a protocolName', () => {
+    const { protocolName } = config;
+    expect(typeof (protocolName)).toBe('string');
+    expect(protocolName).not.toBe('');
+  });
+
+  it('has a protocolAbbreviation', () => {
+    const { protocolAbbreviation } = config;
+    expect(typeof (protocolAbbreviation)).toBe('string');
+    expect(protocolAbbreviation).not.toBe('');
+  });
+
+  it('has a developerAbbreviation', () => {
+    const { developerAbbreviation } = config;
+    expect(typeof (developerAbbreviation)).toBe('string');
+    expect(developerAbbreviation).not.toBe('');
+  });
+
+  it('has a multisig key', () => {
+    const { multisig } = config;
+    expect(typeof (multisig)).toBe('object');
+    expect(multisig).not.toBe({});
+  });
+
+  it('has valid multisig values', () => {
+    const { multisig } = config;
+    const { address, events } = multisig;
+
+    // check that the address is a valid address
+    expect(ethers.utils.isHexString(address, 20)).toBe(true);
+
+    // check that there are events
+    expect(events).not.toBe([]);
+  })
+});
+
+describe('Compound Finance multisig transaction agent', () => {
+
+});
