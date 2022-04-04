2286a47886672a387cea6d871867514dc37ab97d
Darian <darian@arbitraryexecution.com>
2022-04-04 10:30:10 -0700

    initial agent setup

---
+++ multisig-transactions-monitor/src/agent.spec.js
@@ -0,0 +1,12 @@
+const {
+  FindingType,
+  FindingSeverity,
+  Finding,
+  createTransactionEvent,
+  ethers,
+} = require("forta-agent");
+const {
+  handleTransaction,
+} = require("./agent");
+
+describe("Compound Finance multisig transaction agent", () => {});
