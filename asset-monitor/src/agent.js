18b29c8f9db42da1b0f75600560aa2806ec39893
CodeBeholder <93393988+arbitrary-CodeBeholder@users.noreply.github.com>
2022-04-05 12:44:31 -0500

    AE-1033 cToken Asset Monitoring (#6)
    * Initial commit
    
    Currently finds proxies based on configurable proxy-patterns. TODO: check for events from found proxies
    
    * Updated pattern matching
    
    Now uses "some" at the outer layer so that it only needs to match one pattern
    
    * Updated with Event reporting
    
    * Update README.md
    
    * Fixed bugs in finding creation
    
    now properly creates the findings
    also added proxy patterns for all four known cToken proxies and also the most common patterns
    fixed bug with filterLog not finding events with/without an indexed address even though the topic is the same for both
    
    * Added tests
    
    * Update package.json
    
    * Update asset-monitor/README.md
    
    * Updates from PR
    
    Added tests for createUpdateAlert
    Fixed how eventArgs were added to metadata
    Improved how assets were filtered

---
+++ asset-monitor/src/agent.js
@@ -0,0 +1,227 @@
+const {
+  Finding, FindingSeverity, FindingType, ethers, getEthersBatchProvider,
+} = require('forta-agent');
+
+const web3 = require('web3-Eth');
+const web3Eth = new web3();
+
+const {
+  getAbi,
+} = require('./utils');
+
+// load any agent configuration parameters
+const config = require('../agent-config.json');
+
+// set up a variable to hold initialization data used in the handler
+const initializeData = {};
+
+// helper function to create upgrade alerts
+function createUpgradeAlert(
+  protocolName,
+  protocolAbbreviation,
+  developerAbbreviation,
+  cTokenSymbol,
+  cTokenAddress,
+  underlyingAssetAddress,
+  eventArgs,
+  eventType,
+  eventSeverity
+) {
+
+  const modifiedArgs = {}
+  Object.keys(eventArgs).forEach((key) => {
+    modifiedArgs[`eventArgs_${key}`] = eventArgs[key];
+  });
+
+  const finding = Finding.fromObject({
+    name: `${protocolName} cToken Asset Upgraded`,
+    description: `The underlying asset for the ${cTokenSymbol} cToken contract was upgraded`,
+    alertId: `${developerAbbreviation}-${protocolAbbreviation}-CTOKEN-ASSET-UPGRADED`,
+    type: FindingType[eventType],
+    severity: FindingSeverity[eventSeverity],
+    protocol: protocolName,
+    metadata: {
+      cTokenSymbol,
+      cTokenAddress,
+      underlyingAssetAddress,
+        ...modifiedArgs,
+    },
+  });
+  return finding;
+}
+
+function isUpgradeableProxy(asset, proxyPatterns) {
+  let foundPattern = false;
+
+  proxyPatterns.some((pattern) => {
+    let isPattern = pattern.functionHashes.every((functionHash) => {
+      if ( asset.code.indexOf(functionHash) !== -1) {
+        return true;
+      } else {
+        return false;
+      }
+    });
+    if ( isPattern ) {
+      foundPattern = pattern;
+      return true;
+    }
+    else {
+      return false;
+    }
+  });  
+
+  return foundPattern;
+}
+
+async function getUnderlyingAsset(address, abi, provider, proxyPatterns) {
+  let underlyingTokenAddress;
+  let symbol;
+
+  const underlyingAsset = {
+    "isProxy": false
+  };
+
+  const contract = new ethers.Contract(address, abi, provider);
+
+  try {
+    symbol = await contract.symbol();
+    underlyingTokenAddress = await contract.underlying();
+  } catch(e) {
+    console.log(e);
+  }
+
+  if ( underlyingTokenAddress ) {
+    underlyingAsset.symbol = symbol;
+    underlyingAsset.cToken = address;
+    underlyingAsset.address = underlyingTokenAddress;
+    underlyingAsset.code = await provider.getCode(underlyingTokenAddress);
+
+    let proxyPattern = isUpgradeableProxy(underlyingAsset, proxyPatterns);
+    if ( proxyPattern !== false) {
+      underlyingAsset.isProxy = true;
+      underlyingAsset.pattern = proxyPattern;
+    }
+  }
+
+  return underlyingAsset;
+}
+
+async function getCompoundTokens(contract, excludeAddresses) {
+  let cTokenAddresses = await contract.getAllMarkets();
+  cTokenAddresses = cTokenAddresses.map((addr) => addr.toLowerCase());
+  cTokenAddresses = cTokenAddresses.filter((addr) => excludeAddresses.indexOf(addr) === -1);
+
+  return cTokenAddresses;
+}
+
+function provideInitialize(data) {
+  return async function initialize() {
+    // assign configurable fields
+    data.protocolName = config.protocolName;
+    data.protocolAbbreviation = config.protocolAbbreviation;
+    data.developerAbbreviation = config.developerAbbreviation;
+
+    const excludeAddresses = config.excludeAddresses;
+    data.excludeAddresses = excludeAddresses.map((addr) => addr.toLowerCase());
+
+    data.proxyPatterns = config.proxyPatterns;
+    data.proxyPatterns.forEach((pattern) => {
+      pattern.functionHashes = [];
+      pattern.functionSignatures.forEach((signature) => {
+        let hash = web3Eth.abi.encodeFunctionSignature(signature).slice(2);
+        pattern.functionHashes.push(hash);
+      });
+    });
+
+    data.provider = getEthersBatchProvider();
+
+    const {
+      Comptroller: comptroller,
+      cTokens,
+    } = config.contracts;
+
+    // from the Comptroller contract, get all of the cTokens
+    const comptrollerAbi = getAbi(comptroller.abiFile);
+    data.comptrollerContract = new ethers.Contract(
+      comptroller.address,
+      comptrollerAbi,
+      data.provider,
+    );
+
+    data.cTokenAddresses = await getCompoundTokens(data.comptrollerContract, data.excludeAddresses);
+
+    // cToken contracts
+    // gather the underlying asset contracts for every cToken
+    data.cTokenAbi = getAbi(cTokens.abiFile);
+
+    data.upgradeableProxyAssets = [];
+    const promises = data.cTokenAddresses.map(async (address) => {
+      let underlyingAsset = await getUnderlyingAsset(address, data.cTokenAbi, data.provider, data.proxyPatterns);
+      if ( underlyingAsset.isProxy ) data.upgradeableProxyAssets.push(underlyingAsset);
+    });
+    await Promise.all(promises);
+  };
+}
+
+function provideHandleTransaction(data) {
+  return async function handleTransaction(txEvent) {
+    const {
+      provider,
+      cTokenAddresses,
+      cTokenAbi,
+      upgradeableProxyAssets,
+      comptrollerContract,
+      protocolName,
+      protocolAbbreviation,
+      developerAbbreviation,
+      excludeAddresses,
+      proxyPatterns
+    } = data;
+
+    const findings = [];
+
+    // first check that no additional cTokens have been added
+    const currentCTokenAddresses = await getCompoundTokens(comptrollerContract, excludeAddresses);
+    const unique = currentCTokenAddresses.filter((addr) => cTokenAddresses.indexOf(addr) === -1);
+
+    cTokenAddresses.push(...unique);
+
+    if (unique.length > 0) {
+      // create ethers.js Contract Objects and add them to the Object of other Contract Objects
+      const promises = unique.map(async (address) => {
+        let underlyingAsset = await getUnderlyingAsset(address, cTokenAbi, provider, proxyPatterns);
+        if ( underlyingAsset.isProxy ) upgradeableProxyAssets.push(underlyingAsset);
+      });
+      await Promise.all(promises);
+    }
+
+    upgradeableProxyAssets.forEach((asset) => {
+      asset.pattern.eventSignatures.forEach((signature) => {
+        const upgradeEvents = txEvent.filterLog(signature, asset.address);
+        upgradeEvents.forEach((upgradeEvent) => {
+          findings.push(createUpgradeAlert(
+            protocolName,
+            protocolAbbreviation,
+            developerAbbreviation,
+            asset.symbol,
+            asset.cToken,
+            asset.address,
+            upgradeEvent.args,
+            asset.pattern.findingType,
+            asset.pattern.findingSeverity
+          ));
+        });
+      });
+    });
+
+    return findings;
+  };
+}
+
+module.exports = {
+  provideInitialize,
+  initialize: provideInitialize(initializeData),
+  provideHandleTransaction,
+  handleTransaction: provideHandleTransaction(initializeData),
+  createUpgradeAlert
+};
