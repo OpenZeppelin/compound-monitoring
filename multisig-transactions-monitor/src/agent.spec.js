62ee4a7483ce4769a8fa703c63c140e278bae725
darian-ae <100232770+darian-ae@users.noreply.github.com>
2022-04-14 11:58:02 -0700

    AE-1213 Community Multisig Transactions Monitor (#12)

---
+++ multisig-transactions-monitor/src/agent.spec.js
@@ -0,0 +1,367 @@
+const {
+  FindingType,
+  FindingSeverity,
+  Finding,
+  createTransactionEvent,
+  ethers,
+} = require('forta-agent');
+
+// require the agent
+const { provideHandleTransaction, provideInitialize } = require('./agent');
+
+const config = require('../agent-config.json');
+
+const utils = require('./utils');
+
+// check config file
+describe('check agent configuration file', () => {
+  it('has a protocolName', () => {
+    const { protocolName } = config;
+    expect(typeof (protocolName)).toBe('string');
+    expect(protocolName).not.toBe('');
+  });
+
+  it('has a protocolAbbreviation', () => {
+    const { protocolAbbreviation } = config;
+    expect(typeof (protocolAbbreviation)).toBe('string');
+    expect(protocolAbbreviation).not.toBe('');
+  });
+
+  it('has a developerAbbreviation', () => {
+    const { developerAbbreviation } = config;
+    expect(typeof (developerAbbreviation)).toBe('string');
+    expect(developerAbbreviation).not.toBe('');
+  });
+
+  it('has a multisig key and values', () => {
+    const { contracts } = config;
+    const { multisig } = contracts;
+
+    expect(typeof (multisig)).toBe('object');
+    expect(multisig).not.toBe({});
+
+    const { address, events } = multisig;
+
+    // check that the address is a valid address
+    expect(ethers.utils.isHexString(address, 20)).toBe(true);
+
+    // check that there are events
+    expect(events).not.toBe([]);
+  });
+
+  it('has a governance key and values', () => {
+    const { contracts } = config;
+    const { governance } = contracts;
+
+    expect(typeof (governance)).toBe('object');
+    expect(governance).not.toBe({});
+
+    const { address, events } = governance;
+
+    // check that the address is a valid address
+    expect(ethers.utils.isHexString(address, 20)).toBe(true);
+
+    // check that there are events
+    expect(events).not.toBe([]);
+  });
+
+  it('has a comptroller key and values', () => {
+    const { contracts } = config;
+    const { comptroller } = contracts;
+
+    expect(typeof (comptroller)).toBe('object');
+    expect(comptroller).not.toBe({});
+
+    const { address, events } = comptroller;
+
+    // check that the address is a valid address
+    expect(ethers.utils.isHexString(address, 20)).toBe(true);
+
+    // check that there are events
+    expect(events).not.toBe([]);
+  });
+});
+
+// addresses and abi for each contract
+const multisigAddress = '0xbbf3f1421D886E9b2c5D716B5192aC998af2012c';
+const govAddress = '0xc0Da02939E1441F497fd74F78cE7Decb17B66529';
+const comptrollerAddress = '0x3d9819210A31b4961b30EF54bE2aeD79B9c9Cd3B';
+const multisigAbi = utils.getAbi(config.contracts.multisig.abiFile);
+const governanceAbi = utils.getAbi(config.contracts.governance.abiFile);
+const comptrollerAbi = utils.getAbi(config.contracts.comptroller.abiFile);
+
+// create interfaces from abi
+const multisigInterface = new ethers.utils.Interface(multisigAbi.abi);
+const governanceInterface = new ethers.utils.Interface(governanceAbi.abi);
+const comptrollerInterface = new ethers.utils.Interface(comptrollerAbi.abi);
+
+// constants for testing
+const zeroAddress = ethers.constants.AddressZero;
+const zeroHash = ethers.constants.HashZero;
+
+// default empty log structure
+const emptyLog = {
+  address: zeroHash,
+  logIndex: 0,
+  blockNumber: 0,
+  blockHash: zeroHash,
+  transactionIndex: 0,
+  transactionHash: zeroHash,
+  removed: false,
+};
+
+// function to encode default values
+function defaultType(type) {
+  switch (type) {
+    case 'address':
+      return zeroAddress;
+    case 'bool':
+      return false;
+    case 'string':
+      return '';
+    case 'bytes':
+      return '';
+    case 'array':
+      throw new Error('array not implemented');
+    case 'tuple':
+      throw new Error('tuple not implemented');
+    default:
+      return 0;
+  }
+}
+
+// creates log with sparse inputs
+function createLog(eventAbi, inputArgs, logArgs) {
+  const topics = [];
+  const dataTypes = [];
+  const dataValues = [];
+
+  // initialize default log and assign passed in values
+  const log = { ...emptyLog, ...logArgs };
+
+  // build topics and data fields
+  topics.push(ethers.utils.Interface.getEventTopic(eventAbi));
+
+  // parse each input, save into topic or data depending on indexing, may
+  // have to skip if param._isParamType is false, does not support dynamic types
+  eventAbi.inputs.forEach((param) => {
+    const { type } = param;
+    const data = inputArgs[param.name] || defaultType(type);
+    // indexed inputs go into topics, otherwise it goes into data
+    if (param.indexed) {
+      topics.push(ethers.utils.defaultAbiCoder.encode([type], [data]));
+    } else {
+      dataTypes.push(type);
+      dataValues.push(data);
+    }
+  });
+
+  // assign topic and data
+  log.topics = topics;
+  log.data = ethers.utils.defaultAbiCoder.encode(dataTypes, dataValues);
+
+  return log;
+}
+
+describe('monitor multisig contract transactions', () => {
+  describe('handleTransaction', () => {
+    let initializeData;
+    let handleTransaction;
+    let mockTxEvent;
+
+    // monitored events
+    const multisigValidEventName = 'AddedOwner';
+    const governanceValidEventName = 'NewAdmin';
+    const comptrollerValidEventName = 'NewPauseGuardian';
+
+    // non-monitored event for testing
+    const multisigNotMonitoredEventName = 'EnabledModule';
+
+    // random address to pass in as an argument for test cases
+    const testArgumentAddress = '0xa7EbE1285383bf567818EB6622e52782845C0bE2';
+
+    beforeEach(async () => {
+      initializeData = {};
+
+      // initialize handler
+      await (provideInitialize(initializeData))();
+      handleTransaction = provideHandleTransaction(initializeData);
+
+      // create empty transaction event to populate
+      mockTxEvent = createTransactionEvent({});
+    });
+
+    it('returns empty findings if multisig was not involved in a a tranasction', async () => {
+      mockTxEvent.addresses[ethers.constants.AddressZero] = true;
+      const findings = await handleTransaction(mockTxEvent);
+      expect(findings).toStrictEqual([]);
+    });
+
+    it('returns empty findings if multisig was involved in a transaction, but no monitored events were emitted', async () => {
+      mockTxEvent.addresses[multisigAddress] = true;
+      const findings = await handleTransaction(mockTxEvent);
+      expect(findings).toStrictEqual([]);
+    });
+
+    it('returns empty findings if multisig was involved in a transaction, but a non-monitored event was emitted', async () => {
+      mockTxEvent.addresses[multisigAddress] = true;
+
+      // use EnabledModule event to test (not on monitored events list)
+      const log = createLog(
+        multisigInterface.getEvent(multisigNotMonitoredEventName),
+        { owner: zeroAddress },
+        { address: multisigAddress },
+      );
+      mockTxEvent.logs = [log];
+
+      // run agent with mock transaction event
+      const findings = await handleTransaction(mockTxEvent);
+
+      expect(findings).toStrictEqual([]);
+    });
+
+    // tests for multisig events
+    it('returns findings if multisig was involved in a transaction and monitored multisig event was emitted', async () => {
+      mockTxEvent.addresses[multisigAddress] = true;
+
+      // use AddedOwner event to test
+      const log = createLog(
+        multisigInterface.getEvent(multisigValidEventName),
+        { owner: zeroAddress },
+        { address: multisigAddress },
+      );
+      mockTxEvent.logs = [log];
+
+      // run agent with mock transaction event
+      const findings = await handleTransaction(mockTxEvent);
+
+      const expectedFindings = Finding.fromObject({
+        name: 'Compound Multisig Owner Added',
+        description: `Address ${zeroAddress} was added as an owner`,
+        alertId: 'AE-COMP-MULTISIG-OWNER-ADDED-ALERT',
+        protocol: 'Compound',
+        type: FindingType.Info,
+        severity: FindingSeverity.Info,
+        metadata: {
+          owner: zeroAddress,
+          multisigAddress,
+        },
+      });
+
+      expect(findings).toStrictEqual([expectedFindings]);
+    });
+
+    it('returns empty findings if multisig was not involved in a transaction, but a monitored gnosis multisig event was emitted', async () => {
+      mockTxEvent.addresses[testArgumentAddress] = true;
+
+      // use AddedOwner event to test
+      const log = createLog(
+        multisigInterface.getEvent(multisigValidEventName),
+        { owner: zeroAddress },
+        { address: testArgumentAddress },
+      );
+      mockTxEvent.logs = [log];
+
+      // run agent with mock transaction event
+      const findings = await handleTransaction(mockTxEvent);
+
+      expect(findings).toStrictEqual([]);
+    });
+
+    // test for governance events
+    it('returns findings if multisig was involved in a transaction and monitored governance event was emitted', async () => {
+      mockTxEvent.addresses[multisigAddress] = true;
+      mockTxEvent.addresses[govAddress] = true;
+
+      // use NewAdmin event to test
+      const log = createLog(
+        governanceInterface.getEvent(governanceValidEventName),
+        { oldAmin: zeroAddress, newAdmin: testArgumentAddress },
+        { address: govAddress },
+      );
+      mockTxEvent.logs = [log];
+
+      const findings = await handleTransaction(mockTxEvent);
+      const expectedFindings = Finding.fromObject({
+        name: 'Compound New Adimn',
+        description: `Governance Admin changed from ${zeroAddress} to ${testArgumentAddress}`,
+        alertId: 'AE-COMP-GOVERNANCE-NEW-ADMIN-ALERT',
+        protocol: 'Compound',
+        type: FindingType.Info,
+        severity: FindingSeverity.Info,
+        metadata: {
+          oldAdmin: zeroAddress,
+          newAdmin: testArgumentAddress,
+          multisigAddress,
+        },
+      });
+
+      expect(findings).toStrictEqual([expectedFindings]);
+    });
+
+    it('returns empty findings if multisig was not involved in a transaction, but a monitored governance event was emitted', async () => {
+      mockTxEvent.addresses[govAddress] = true;
+
+      // use NewAdmin event to test
+      const log = createLog(
+        governanceInterface.getEvent(governanceValidEventName),
+        { oldAmin: zeroAddress, newAdmin: testArgumentAddress },
+        { address: govAddress },
+      );
+      mockTxEvent.logs = [log];
+
+      const findings = await handleTransaction(mockTxEvent);
+
+      expect(findings).toStrictEqual([]);
+    });
+
+    // test for comptroller events
+    it('returns findings if multisig was involved in a transaction and monitored comptroller event was emitted', async () => {
+      mockTxEvent.addresses[multisigAddress] = true;
+      mockTxEvent.addresses[comptrollerAddress] = true;
+
+      // use NewPauseGuardian event to test
+      const log = createLog(
+        comptrollerInterface.getEvent(comptrollerValidEventName),
+        { oldPauseGuardian: testArgumentAddress, newPauseGuardian: zeroAddress },
+        { address: comptrollerAddress },
+      );
+
+      mockTxEvent.logs = [log];
+
+      const findings = await handleTransaction(mockTxEvent);
+      const expectedFindings = Finding.fromObject({
+        name: 'Compound New Pause Guardian',
+        description: `Pause Guardian changed from ${testArgumentAddress} to ${zeroAddress}`,
+        alertId: 'AE-COMP-NEW-PAUSE-GUARDIAN-ALERT',
+        protocol: 'Compound',
+        type: FindingType.Info,
+        severity: FindingSeverity.Info,
+        metadata: {
+          oldPauseGuardian: testArgumentAddress,
+          newPauseGuardian: zeroAddress,
+          multisigAddress,
+        },
+      });
+
+      expect(findings).toStrictEqual([expectedFindings]);
+    });
+
+    it('returns empty findings if multisig was not involved in a transaction, but a monitored comptroller event was emitted', async () => {
+      mockTxEvent.addresses[comptrollerAddress] = true;
+
+      // use NewAdmin event to test
+      const log = createLog(
+        comptrollerInterface.getEvent(comptrollerValidEventName),
+        { oldPauseGuardian: testArgumentAddress, newPauseGuardian: zeroAddress },
+        { address: comptrollerAddress },
+      );
+
+      mockTxEvent.logs = [log];
+
+      const findings = await handleTransaction(mockTxEvent);
+
+      expect(findings).toStrictEqual([]);
+    });
+  });
+});
---
+++ multisig-transactions-monitor/src/utils.js
@@ -0,0 +1,468 @@
+/* eslint-disable global-require */
+const { Finding, FindingType, FindingSeverity } = require('forta-agent');
+
+const { contracts: { multisig: { address: multisigAddress } } } = require('../agent-config.json');
+
+/// CREATE FINDINGS FOR GNOSIS SAFE INTERACTIONS ///
+/*
+        "AddedOwner",
+        "RemovedOwner",
+*/
+function createAddOwnerFinding(
+  log,
+  protocolName,
+  protocolAbbreviation,
+  developerAbbreviation,
+) {
+  const { owner } = log.args;
+
+  const finding = Finding.fromObject({
+    name: `${protocolName} Multisig Owner Added`,
+    description: `Address ${owner} was added as an owner`,
+    alertId: `${developerAbbreviation}-${protocolAbbreviation}-MULTISIG-OWNER-ADDED-ALERT`,
+    protocol: protocolName,
+    type: FindingType.Info,
+    severity: FindingSeverity.Info,
+    metadata: {
+      owner,
+      multisigAddress,
+    },
+  });
+  return finding;
+}
+
+function createRemoveOwnerFinding(
+  log,
+  protocolName,
+  protocolAbbreviation,
+  developerAbbreviation,
+) {
+  const { owner } = log.args;
+
+  const finding = Finding.fromObject({
+    name: `${protocolName} Multisig Owner Removed`,
+    description: `Address ${owner} was removed as an owner`,
+    alertId: `${developerAbbreviation}-${protocolAbbreviation}-MULTISIG-OWNER-REMOVED-ALERT`,
+    protocol: protocolName,
+    type: FindingType.Info,
+    severity: FindingSeverity.Info,
+    metadata: {
+      owner,
+      multisigAddress,
+    },
+  });
+  return finding;
+}
+
+function createGnosisFinding(
+  log,
+  protocolName,
+  protocolAbbreviation,
+  developerAbbreviation,
+) {
+  let finding;
+
+  // iterate over logs to see which action occured
+  if (log.name === 'AddedOwner') {
+    finding = createAddOwnerFinding(
+      log,
+      protocolName,
+      protocolAbbreviation,
+      developerAbbreviation,
+    );
+  }
+
+  if (log.name === 'RemovedOwner') {
+    finding = createRemoveOwnerFinding(
+      log,
+      protocolName,
+      protocolAbbreviation,
+      developerAbbreviation,
+    );
+  }
+
+  return finding;
+}
+
+/// CREATE FINDINGS FOR GOVERNOR BRAVO INTERACTIONS ///
+
+/*
+        "ProposalCreated",
+        "ProposalExecuted",
+        "ProposalCanceled",
+        "VoteCast",
+        "ProposalThresholdSet",
+        "NewAdmin"
+*/
+
+function createProposalCreatedFinding(
+  log,
+  protocolName,
+  protocolAbbreviation,
+  developerAbbreviation,
+) {
+  const proposalId = log.args.id.toString();
+
+  const finding = Finding.fromObject({
+    name: `${protocolName} Proposal Created`,
+    description: `Governance Proposal ${proposalId} was just created by multisig ${multisigAddress}`,
+    alertId: `${developerAbbreviation}-${protocolAbbreviation}-GOVERNANCE-PROPOSAL-CREATED-ALERT`,
+    protocol: protocolName,
+    type: FindingType.Info,
+    severity: FindingSeverity.Info,
+    metadata: {
+      proposalId,
+      multisigAddress,
+    },
+  });
+  return finding;
+}
+
+function createProposalExecutedFinding(
+  log,
+  protocolName,
+  protocolAbbreviation,
+  developerAbbreviation,
+) {
+  const proposalId = log.args.id.toString();
+
+  const finding = Finding.fromObject({
+    name: `${protocolName} Proposal Executed`,
+    description: `Governance Proposal ${proposalId} was just executed by multisig ${multisigAddress}`,
+    alertId: `${developerAbbreviation}-${protocolAbbreviation}-GOVERNANCE-PROPOSAL-EXECUTED-ALERT`,
+    protocol: protocolName,
+    type: FindingType.Info,
+    severity: FindingSeverity.Info,
+    metadata: {
+      proposalId,
+      multisigAddress,
+    },
+  });
+  return finding;
+}
+
+function createProposalCanceledFinding(
+  log,
+  protocolName,
+  protocolAbbreviation,
+  developerAbbreviation,
+) {
+  const proposalId = log.args.id.toString();
+
+  const finding = Finding.fromObject({
+    name: `${protocolName} Proposal Canceled`,
+    description: `Governance Proposal ${proposalId} was just canceled by multisig ${multisigAddress}`,
+    alertId: `${developerAbbreviation}-${protocolAbbreviation}-GOVERNANCE-PROPOSAL-CANCELED-ALERT`,
+    protocol: protocolName,
+    type: FindingType.Info,
+    severity: FindingSeverity.Info,
+    metadata: {
+      proposalId,
+      multisigAddress,
+    },
+  });
+  return finding;
+}
+
+function createVoteCastFinding(
+  log,
+  protocolName,
+  protocolAbbreviation,
+  developerAbbreviation,
+) {
+  const proposalId = log.args.proposalId.toString();
+
+  const finding = Finding.fromObject({
+    name: `${protocolName} Proposal Vote Casted`,
+    description: `Governance Proposal id ${proposalId} was voted on by multisig ${multisigAddress}`,
+    alertId: `${developerAbbreviation}-${protocolAbbreviation}-GOVERNANCE-VOTE-CAST-ALERT`,
+    protocol: protocolName,
+    type: FindingType.Info,
+    severity: FindingSeverity.Info,
+    metadata: {
+      proposalId,
+      multisigAddress,
+    },
+  });
+  return finding;
+}
+
+function createProposalThresholdSetFinding(
+  log,
+  protocolName,
+  protocolAbbreviation,
+  developerAbbreviation,
+) {
+  const oldThreshold = log.args.oldProposalThreshold.toString();
+  const newThreshold = log.args.newProposalThreshold.toString();
+
+  const finding = Finding.fromObject({
+    name: `${protocolName} Proposal Threshold Set`,
+    description: `Governance Proposal threshold changed from ${oldThreshold} to ${newThreshold}`,
+    alertId: `${developerAbbreviation}-${protocolAbbreviation}-GOVERNANCE-THRESHOLD-SET-ALERT`,
+    protocol: protocolName,
+    type: FindingType.Info,
+    severity: FindingSeverity.Info,
+    metadata: {
+      oldThreshold,
+      newThreshold,
+      multisigAddress,
+    },
+  });
+  return finding;
+}
+
+function createNewAdminFinding(
+  log,
+  protocolName,
+  protocolAbbreviation,
+  developerAbbreviation,
+) {
+  const { oldAdmin, newAdmin } = log.args;
+
+  const finding = Finding.fromObject({
+    name: `${protocolName} New Adimn`,
+    description: `Governance Admin changed from ${oldAdmin} to ${newAdmin}`,
+    alertId: `${developerAbbreviation}-${protocolAbbreviation}-GOVERNANCE-NEW-ADMIN-ALERT`,
+    protocol: protocolName,
+    type: FindingType.Info,
+    severity: FindingSeverity.Info,
+    metadata: {
+      oldAdmin,
+      newAdmin,
+      multisigAddress,
+    },
+  });
+  return finding;
+}
+
+function createGovernanceFinding(
+  log,
+  protocolName,
+  protocolAbbreviation,
+  developerAbbreviation,
+) {
+  let finding;
+
+  if (log.name === 'ProposalCreated') {
+    finding = createProposalCreatedFinding(
+      log,
+      protocolName,
+      protocolAbbreviation,
+      developerAbbreviation,
+    );
+  }
+
+  if (log.name === 'ProposalExecuted') {
+    finding = createProposalExecutedFinding(
+      log,
+      protocolName,
+      protocolAbbreviation,
+      developerAbbreviation,
+    );
+  }
+
+  if (log.name === 'ProposalCanceled') {
+    finding = createProposalCanceledFinding(
+      log,
+      protocolName,
+      protocolAbbreviation,
+      developerAbbreviation,
+    );
+  }
+
+  if (log.name === 'VoteCast') {
+    finding = createVoteCastFinding(
+      log,
+      protocolName,
+      protocolAbbreviation,
+      developerAbbreviation,
+    );
+  }
+
+  if (log.name === 'ProposalThresHoldSet') {
+    finding = createProposalThresholdSetFinding(
+      log,
+      protocolName,
+      protocolAbbreviation,
+      developerAbbreviation,
+    );
+  }
+
+  if (log.name === 'NewAdmin') {
+    finding = createNewAdminFinding(
+      log,
+      protocolName,
+      protocolAbbreviation,
+      developerAbbreviation,
+    );
+  }
+
+  return finding;
+}
+
+/// CREATE FINDINGS FOR COMPTROLLER INTERACTIONS ///
+
+/*
+        "NewPauseGuardian",
+        "ActionPaused",
+        "NewBorrowCap",
+        "NewBorrowCapGuardian",
+        "NewPauseGuardian"
+*/
+
+function createNewPauseGuardianFinding(
+  log,
+  protocolName,
+  protocolAbbreviation,
+  developerAbbreviation,
+) {
+  const { oldPauseGuardian, newPauseGuardian } = log.args;
+
+  const finding = Finding.fromObject({
+    name: `${protocolName} New Pause Guardian`,
+    description: `Pause Guardian changed from ${oldPauseGuardian} to ${newPauseGuardian}`,
+    alertId: `${developerAbbreviation}-${protocolAbbreviation}-NEW-PAUSE-GUARDIAN-ALERT`,
+    protocol: protocolName,
+    type: FindingType.Info,
+    severity: FindingSeverity.Info,
+    metadata: {
+      oldPauseGuardian,
+      newPauseGuardian,
+      multisigAddress,
+    },
+  });
+  return finding;
+}
+
+function createActionPausedFinding(
+  log,
+  protocolName,
+  protocolAbbreviation,
+  developerAbbreviation,
+) {
+  const { action } = log.args;
+
+  const finding = Finding.fromObject({
+    name: `${protocolName} Action Paused`,
+    description: `Action ${action} was Paused by multisig ${multisigAddress}`,
+    alertId: `${developerAbbreviation}-${protocolAbbreviation}-ACTION-PAUSED-ALERT`,
+    protocol: protocolName,
+    type: FindingType.Info,
+    severity: FindingSeverity.Info,
+    metadata: {
+      action,
+      multisigAddress,
+    },
+  });
+  return finding;
+}
+
+function createNewBorrowCapFinding(
+  log,
+  protocolName,
+  protocolAbbreviation,
+  developerAbbreviation,
+) {
+  const { cToken, newBorrowCap } = log.args;
+
+  const finding = Finding.fromObject({
+    name: `${protocolName} New Borrow Cap`,
+    description: `New Borrow Cap for cToken ${cToken} with a borrow cap of ${newBorrowCap} was set by multisig ${multisigAddress}`,
+    alertId: `${developerAbbreviation}-${protocolAbbreviation}-NEW-BORROW-CAP-ALERT`,
+    protocol: protocolName,
+    type: FindingType.Info,
+    severity: FindingSeverity.Info,
+    metadata: {
+      cToken,
+      newBorrowCap,
+      multisigAddress,
+    },
+  });
+  return finding;
+}
+
+function createNewBorrowCapGaurdianFinding(
+  log,
+  protocolName,
+  protocolAbbreviation,
+  developerAbbreviation,
+) {
+  const { oldBorrowCapGuardian, newBorrowCapGuardian } = log.args;
+
+  const finding = Finding.fromObject({
+    name: `${protocolName} New Borrow Cap Guardian`,
+    description: `Borrow Guardian changed from ${oldBorrowCapGuardian} to ${newBorrowCapGuardian}`,
+    alertId: `${developerAbbreviation}-${protocolAbbreviation}-NEW-BORROW-CAP-GUARDIAN-ALERT`,
+    protocol: protocolName,
+    type: FindingType.Info,
+    severity: FindingSeverity.Info,
+    metadata: {
+      oldBorrowCapGuardian,
+      newBorrowCapGuardian,
+      multisigAddress,
+    },
+  });
+  return finding;
+}
+
+function createComptrollerFinding(
+  log,
+  protocolName,
+  protocolAbbreviation,
+  developerAbbreviation,
+) {
+  let finding;
+
+  if (log.name === 'NewBorrowCapGuardian') {
+    finding = createNewBorrowCapGaurdianFinding(
+      log,
+      protocolName,
+      protocolAbbreviation,
+      developerAbbreviation,
+    );
+  }
+
+  if (log.name === 'NewBorrowCap') {
+    finding = createNewBorrowCapFinding(
+      log,
+      protocolName,
+      protocolAbbreviation,
+      developerAbbreviation,
+    );
+  }
+
+  if (log.name === 'ActionPaused') {
+    finding = createActionPausedFinding(
+      log,
+      protocolName,
+      protocolAbbreviation,
+      developerAbbreviation,
+    );
+  }
+
+  if (log.name === 'NewPauseGuardian') {
+    finding = createNewPauseGuardianFinding(
+      log,
+      protocolName,
+      protocolAbbreviation,
+      developerAbbreviation,
+    );
+  }
+  return finding;
+}
+
+function getAbi(abiFile) {
+  // eslint-disable-next-line import/no-dynamic-require
+  const abi = require(`../abi/${abiFile}`);
+  return abi;
+}
+
+module.exports = {
+  createAddOwnerFinding,
+  createRemoveOwnerFinding,
+  getAbi,
+  createGnosisFinding,
+  createGovernanceFinding,
+  createComptrollerFinding,
+};
