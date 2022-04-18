81a1fcb45866a97c6f4062cc7c0f8101a472b4b8
echo <echo@arbitraryexecution.com>
2022-04-18 16:30:19 -0400

    AE-1156: Added scripts for approving and executing transaction.  Updated proposal script to include gasPrice value.

---
+++ gnosis-safe-deploy/scripts/executeTransaction.js
@@ -0,0 +1,78 @@
+const EthersAdapter = require('@gnosis.pm/safe-ethers-lib')["default"];
+const { default: Safe, EthSignSignature } = require('@gnosis.pm/safe-core-sdk');
+const SafeServiceClient = require('@gnosis.pm/safe-service-client')["default"];
+const fortaAgent = require('forta-agent');
+
+const { ethers } = fortaAgent;
+
+require('dotenv').config();
+
+const polygonSafeAddress = process.env.POLYGON_SAFE_ADDRESS;
+const polygonEndpoint = process.env.POLYGON_ENDPOINT;
+
+// load the private key for the account that will be signing the transaction
+const ownerPrivateKey = process.env.OWNER_TWO_PRIVATE_KEY;
+
+// load the transaction hash to approve
+const safeTxHash = process.env.SAFE_TX_HASH;
+
+const provider = new ethers.providers.JsonRpcProvider(polygonEndpoint);
+
+// create a wallet (signer) and connect it to the provider
+const safeWallet = new ethers.Wallet(ownerPrivateKey, provider);
+const signer = safeWallet.connect(provider);
+
+const ethAdapter = new EthersAdapter({
+  ethers,
+  signer,
+  provider,
+});
+
+// initialize the Safe Service Client
+const transactionServiceUrl = 'https://safe-transaction.polygon.gnosis.io/';
+const safeService = new SafeServiceClient({
+  txServiceUrl: transactionServiceUrl,
+  ethAdapter,
+});
+
+async function main() {
+  console.log('Getting deployed Gnosis Safe Contract');
+  const safeSdk = await Safe.create({
+      ethAdapter,
+      safeAddress: polygonSafeAddress,
+  });
+
+  console.log('Getting pending transactions');
+  const transaction = await safeService.getTransaction(safeTxHash);
+  console.log(transaction);
+
+  const safeTransactionData = {
+    to: transaction.to,
+    value: transaction.value,
+    data: transaction.data,
+    operation: transaction.operation,
+    safeTxGas: transaction.safeTxGas,
+    baseGas: transaction.baseGas,
+    gasPrice: transaction.gasPrice,
+    gasToken: transaction.gasToken,
+    refundReceiver: transaction.refundReceiver,
+    nonce: transaction.nonce
+  }
+
+  const safeTransaction = await safeSdk.createTransaction(safeTransactionData);
+  transaction.confirmations.forEach(confirmation => {
+    const signature = new EthSignSignature(confirmation.owner, confirmation.signature);
+    safeTransaction.addSignature(signature);
+  });
+
+  const executeTxResponse = await safeSdk.executeTransaction(safeTransaction);
+  const receipt = executeTxResponse.transactionResponse && (await executeTxResponse.transactionResponse.wait());
+  console.log(`Receipt: ${JSON.stringify(receipt, null, 2)}`);
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
