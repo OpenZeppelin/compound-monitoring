31125807d871be25ba4f1ab6121d0217b511ce87
Nomad-AE <nomad@arbitraryexecution.com>
2022-04-28 19:15:20 +0900

    AE-1247: removed TS function

--- liquidation-monitor/src/utils.js
+++ liquidation-monitor/src/utils.js
@@ -6,16 +6,6 @@ function getAbi(abiName) {
   return abi;
 }
 
-// Time-stamping + Troubleshooting function
-function ts(input) {
-  const today = new Date();
-  const hours = String(today.getHours()).padStart(2, '0');
-  const minutes = String(today.getMinutes()).padStart(2, '0');
-  const seconds = String(today.getSeconds()).padStart(2, '0');
-  const time = `${hours}:${minutes}:${seconds}`;
-  console.log('TS -', time, input);
-}
-
 // Helper for Compound API
 function buildJsonRequest(maxHealth, minBorrow, pageNumber, pageSize) {
   const jsonRequest = {
@@ -29,8 +19,9 @@ function buildJsonRequest(maxHealth, minBorrow, pageNumber, pageSize) {
   return jsonRequest;
 }
 
-async function callCompoundAPI(url, jsonRequest) {
-  const response = await fetch(url, {
+async function callCompoundAPI(jsonRequest) {
+  const apiURL = 'https://api.compound.finance/api/v2/account';
+  const response = await fetch(apiURL, {
     method: 'POST',
     body: JSON.stringify(jsonRequest),
   });
@@ -38,7 +29,7 @@ async function callCompoundAPI(url, jsonRequest) {
   if (!response.ok) {
     console.error('Got non-2XX response from API server.');
   }
-  
+
   let responseData;
   try {
     responseData = await response.json();
@@ -50,7 +41,6 @@ async function callCompoundAPI(url, jsonRequest) {
 
 module.exports = {
   getAbi,
-  ts,
   callCompoundAPI,
   buildJsonRequest,
 };
