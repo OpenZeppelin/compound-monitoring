e0b4c4e6d8a1134633a4fe00ae4c22d32e90190a
Nomad-AE <nomad@arbitraryexecution.com>
2022-05-02 22:55:48 +0900

    AE-1247: finished config tests

--- liquidation-monitor/src/agent.spec.js
+++ liquidation-monitor/src/agent.spec.js
@@ -1,3 +1,5 @@
+const BigNumber = require('bignumber.js');
+
 // Configurable properties
 // All prices are in ETH
 const mockBtcPrice = 11;
@@ -6,8 +8,11 @@ const mockUsdcPrice = 0.00033;
 // Same decimals for all tokens
 const mockDecimals = 18;
 
-const BigNumber = require('bignumber.js');
+const {
+  ethers, TransactionEvent, Finding, FindingType, FindingSeverity,
+} = require('forta-agent');
 const config = require('../agent-config.json');
+
 const {
   getAbi, callCompoundAPI, buildJsonRequest,
 } = require('./utils');
@@ -41,17 +46,9 @@ jest.mock('forta-agent', () => ({
   getEthersProvider: jest.fn(),
   ethers: {
     ...jest.requireActual('ethers'),
-    Contract: jest.fn().mockReturnValue(mockERC20Contract),
+    Contract: jest.fn(), // .mockReturnValue(mockERC20Contract),
   },
 }));
-
-const {
-  FindingType,
-  FindingSeverity,
-  Finding,
-  createTransactionEvent,
-  ethers,
-} = require('forta-agent');
 
 // utility function specific for this test module
 // we are intentionally not using the Forta SDK function due to issues with
@@ -69,65 +66,167 @@ function createTransactionEvent(txObject) {
   return txEvent;
 }
 
+// check the configuration file to verify the values
+describe('check agent configuration file', () => {
+  it('developerAbbreviation key required', () => {
+    const { developerAbbreviation } = config;
+    expect(typeof (developerAbbreviation)).toBe('string');
+    expect(developerAbbreviation).not.toBe('');
+  });
 
+  it('protocolName key required', () => {
+    const { protocolName } = config;
+    expect(typeof (protocolName)).toBe('string');
+    expect(protocolName).not.toBe('');
+  });
 
+  it('protocolAbbreviation key required', () => {
+    const { protocolAbbreviation } = config;
+    expect(typeof (protocolAbbreviation)).toBe('string');
+    expect(protocolAbbreviation).not.toBe('');
+  });
 
+  it('liquidationMonitor key required', () => {
+    const { liquidationMonitor } = config;
+    expect(typeof (liquidationMonitor)).toBe('object');
+    expect(liquidationMonitor).not.toBe({});
+  });
 
-describe('high tether transfer agent', () => {
-  describe('handleTransaction', () => {
-    const mockTxEvent = createTransactionEvent({});
-    mockTxEvent.filterLog = jest.fn();
+  it('comptrollerAddress key required', () => {
+    const { liquidationMonitor: { comptrollerAddress } } = config;
+    expect(typeof (comptrollerAddress)).toBe('string');
+    expect(comptrollerAddress).not.toBe('');
+  });
 
-    beforeEach(() => {
-      mockTxEvent.filterLog.mockReset();
-    });
+  it('comptrollerABI key required', () => {
+    const { liquidationMonitor: { comptrollerABI } } = config;
+    expect(typeof (comptrollerABI)).toBe('string');
+    expect(comptrollerABI).not.toBe('');
+  });
 
-    it('returns empty findings if there are no Tether transfers', async () => {
-      mockTxEvent.filterLog.mockReturnValue([]);
+  it('oneInchAddress key required', () => {
+    const { liquidationMonitor: { oneInchAddress } } = config;
+    expect(typeof (oneInchAddress)).toBe('string');
+    expect(oneInchAddress).not.toBe('');
+  });
 
-      const findings = await handleTransaction(mockTxEvent);
+  it('comptrollerAddress key required', () => {
+    const { liquidationMonitor: { comptrollerAddress } } = config;
+    expect(typeof (comptrollerAddress)).toBe('string');
+    expect(comptrollerAddress).not.toBe('');
+  });
 
-      expect(findings).toStrictEqual([]);
-      expect(mockTxEvent.filterLog).toHaveBeenCalledTimes(1);
-      expect(mockTxEvent.filterLog).toHaveBeenCalledWith(
-        ERC20_TRANSFER_EVENT,
-        TETHER_ADDRESS,
-      );
-    });
+  it('triggerLevels key required', () => {
+    const { liquidationMonitor: { triggerLevels } } = config;
+    expect(typeof (triggerLevels)).toBe('object');
+    expect(triggerLevels).not.toBe({});
+  });
 
-    it('returns a finding if there is a Tether transfer over 10,000', async () => {
-      const mockTetherTransferEvent = {
-        args: {
-          from: '0xabc',
-          to: '0xdef',
-          value: ethers.BigNumber.from('20000000000'), // 20k with 6 decimals
-        },
-      };
-      mockTxEvent.filterLog.mockReturnValue([mockTetherTransferEvent]);
+  it('maximumHealth key required', () => {
+    const { liquidationMonitor: { triggerLevels: { maximumHealth } } } = config;
+    expect(typeof (maximumHealth)).toBe('number');
+    expect(maximumHealth).not.toBe('');
+  });
 
-      const findings = await handleTransaction(mockTxEvent);
+  it('minimumBorrowInETH key required', () => {
+    const { liquidationMonitor: { triggerLevels: { minimumBorrowInETH } } } = config;
+    expect(typeof (minimumBorrowInETH)).toBe('number');
+    expect(minimumBorrowInETH).not.toBe('');
+  });
 
-      const normalizedValue = mockTetherTransferEvent.args.value.div(
-        10 ** TETHER_DECIMALS,
-      );
-      expect(findings).toStrictEqual([
-        Finding.fromObject({
-          name: 'High Tether Transfer',
-          description: `High amount of USDT transferred: ${normalizedValue}`,
-          alertId: 'FORTA-1',
-          severity: FindingSeverity.Low,
-          type: FindingType.Info,
-          metadata: {
-            to: mockTetherTransferEvent.args.to,
-            from: mockTetherTransferEvent.args.from,
-          },
-        }),
-      ]);
-      expect(mockTxEvent.filterLog).toHaveBeenCalledTimes(1);
-      expect(mockTxEvent.filterLog).toHaveBeenCalledWith(
-        ERC20_TRANSFER_EVENT,
-        TETHER_ADDRESS,
-      );
-    });
+  it('minimumLiquidationInUSD key required', () => {
+    const { liquidationMonitor: { triggerLevels: { minimumLiquidationInUSD } } } = config;
+    expect(typeof (minimumLiquidationInUSD)).toBe('number');
+    expect(minimumLiquidationInUSD).not.toBe('');
+  });
+
+  it('lowHealthThreshold key required', () => {
+    const { liquidationMonitor: { triggerLevels: { lowHealthThreshold } } } = config;
+    expect(typeof (lowHealthThreshold)).toBe('number');
+    expect(lowHealthThreshold).not.toBe('');
+  });
+
+  it('alert key required', () => {
+    const { liquidationMonitor: { alert } } = config;
+    expect(typeof (alert)).toBe('object');
+    expect(alert).not.toBe({});
+  });
+
+  it('alert type value must be valid', () => {
+    const { liquidationMonitor: { alert: { type } } } = config;
+    expect(typeof (type)).toBe('string');
+    expect(type).not.toBe('');
+    // check type, this will fail if the value of type is not valid
+    expect(Object.prototype.hasOwnProperty.call(FindingType, type)).toBe(true);
+  });
+
+  it('alert severity value must be valid', () => {
+    const { liquidationMonitor: { alert: { severity } } } = config;
+    expect(typeof (severity)).toBe('string');
+    expect(severity).not.toBe('');
+    // check severity, this will fail if the value of severity is not valid
+    expect(Object.prototype.hasOwnProperty.call(FindingSeverity, severity)).toBe(true);
   });
 });
+
+// agent tests
+// describe('handleTransaction', () => {
+//   const {
+//     developerAbbreviation,
+//     protocolName,
+//     protocolAbbreviation,
+//     cCOMPAddress,
+//     borrowLevels,
+//   } = config;
+//   const iface = new ethers.utils.Interface(mockERC20Contract);
+//   const mockBorrowerAddress = '0x1212121212121212121212121212121212121212';
+//   let initializeData;
+//   let handleTransaction;
+
+//   beforeEach(async () => {
+//     initializeData = {};
+//     // initialize the handler
+//     await (provideInitialize(initializeData))();
+//     handleTransaction = provideHandleTransaction(initializeData);
+//     mockERC20Contract.balanceOf.mockReset();
+//   });
+
+//   it('returns no findings if no borrow event was emitted and no interaction with the cCOMP token occurred', async () => {
+//     const mockReceipt = {
+//       logs: [],
+//     };
+//     // create the mock txEvent
+//     const txEvent = new TransactionEvent(null, null, null, mockReceipt, [], [], null);
+
+//     // run the agent
+//     const findings = await handleTransaction(txEvent);
+//     expect(findings).toStrictEqual([]);
+//     expect(mockERC20Contract.balanceOf).toHaveBeenCalledTimes(0);
+//   });
+
+//   it('returns no findings if an event other than the borrow event is emitted from the cCOMP token contract', async () => {
+//     // build mock receipt for mock txEvent, in this case the log event topics will not correspond to
+//     // the Borrow event so we should not expect to see a finding
+//     const mockTopics = [
+//       ethers.utils.id('mockEvent(indexed address)'),
+//       ethers.utils.defaultAbiCoder.encode(
+//         ['address'], ['0x1111111111111111111111111111111111111111'],
+//       ),
+//     ];
+//     const mockReceipt = {
+//       logs: [{
+//         address: cCOMPAddress,
+//         topics: mockTopics,
+//         data: '0x',
+//       }],
+//     };
+
+//     // create the mock txEvent
+//     const txEvent = new TransactionEvent(null, null, null, mockReceipt, [], [], null);
+
+//     // run the agent
+//     const findings = await handleTransaction(txEvent);
+//     expect(findings).toStrictEqual([]);
+//     expect(mockERC20Contract.balanceOf).toHaveBeenCalledTimes(0);
+//   });
+// });
