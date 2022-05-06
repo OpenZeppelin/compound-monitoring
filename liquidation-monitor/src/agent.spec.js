688f03e28a3d2393f24a036ee01cad8c63e1282f
Nomad-AE <nomad@arbitraryexecution.com>
2022-05-07 01:37:30 +0900

    AE-1247: new testing approach

--- liquidation-monitor/src/agent.spec.js
+++ liquidation-monitor/src/agent.spec.js
@@ -1,4 +1,5 @@
 const BigNumber = require('bignumber.js');
+const axios = require('axios');
 
 // Configurable properties
 // All prices are in ETH
@@ -8,31 +9,17 @@ const mockUsdcPrice = 0.00033;
 // Same decimals for all tokens
 const mockDecimals = 18;
 
-const {
-  ethers, TransactionEvent, Finding, FindingType, FindingSeverity,
-} = require('forta-agent');
-const config = require('../agent-config.json');
-
-const {
-  getAbi, callCompoundAPI, buildJsonRequest,
-} = require('./utils');
-
-const { provideHandleTransaction, provideInitialize, provideHandleBlock } = require('./agent');
-
 const decimals = new BigNumber(10).pow(mockDecimals);
 
-const mockComptrollerContract = {
+const mockContract = {
+  // Comptroller
   getAssetsIn: jest.fn().mockResolvedValue(mockDecimals),
-  markets: jest.fn(), //returns collateral factor
+  markets: jest.fn(), // returns collateral factor
   getAccountLiquidity: jest.fn(),
   quorumVotes: jest.fn(),
-};
-
-const mockOneInchContract = {
+  // OneInch
   getRateToEth: jest.fn(),
-};
-
-const mockERC20Contract = {
+  // ERC20
   decimals: jest.fn().mockResolvedValue(mockDecimals),
   getAccountSnapshot: jest.fn(),
   symbol: jest.fn(),
@@ -46,9 +33,22 @@ jest.mock('forta-agent', () => ({
   getEthersProvider: jest.fn(),
   ethers: {
     ...jest.requireActual('ethers'),
-    Contract: jest.fn(), // .mockReturnValue(mockERC20Contract),
+    Contract: jest.fn().mockReturnValue(mockContract),
   },
 }));
+
+const {
+  ethers, TransactionEvent, Finding, FindingType, FindingSeverity, getEthersProvider,
+} = require('forta-agent');
+const config = require('../agent-config.json');
+
+const {
+  getAbi, callCompoundAPI, buildJsonRequest,
+} = require('./utils');
+
+const {
+  provideHandleTransaction, provideInitialize, provideHandleBlock, verifyToken,
+} = require('./agent');
 
 // utility function specific for this test module
 // we are intentionally not using the Forta SDK function due to issues with
@@ -170,28 +170,102 @@ describe('check agent configuration file', () => {
 });
 
 // agent tests
-describe('handleTransaction', () => {
+describe('handleBlock', () => {
   const {
     developerAbbreviation,
     protocolName,
     protocolAbbreviation,
-    cCOMPAddress,
-    borrowLevels,
   } = config;
-  // const iface = new ethers.utils.Interface(mockERC20Contract);
+
   const mockBorrowerAddress = '0x1111';
-  let initializeData;
+  let data;
   let handleTransaction;
 
+  async function handleMockBlockEvent() {
+    // Check all account healths
+    const { accounts } = data;
+    data.accounts.forEach((currentAccount) => {
+      let supplied = 0;
+      let borrowed = 0;
+      Object.keys(data.tokens).forEach((token) => {
+        try {
+          borrowed += data.borrow.token.currentAccount * data.token.price;
+        } catch (err) { /* pass */ }
+        try {
+          supplied += data.supplied.token.currentAccount * data.token.price;
+        } catch (err) { /* pass */ }
+      });
+    });
+    const shortfallUSD = BigNumber(ethers.utils.formatEther(liquidity[2]).toString());
+
+    // Health factor affects the liquidatable amount. Ex: Shortfall of $50 with a Health factor
+    // of 0.50 means that only $25 can be successfully liquidated. ( $25 supplied / $50 borrowed )
+    const liquidationAmount = shortfallUSD.multipliedBy(accounts[currentAccount].health);
+
+    // Create a finding if the liquidatable amount is below the threshold
+    // Shorten metadata to 2 decimal places
+    if (liquidationAmount.isGreaterThan(data.minimumLiquidationInUSD)) {
+      const newFinding = createAlert(
+        data.developerAbbreviation,
+        data.protocolName,
+        data.protocolAbbreviation,
+        data.alert.type,
+        data.alert.severity,
+        currentAccount,
+        liquidationAmount.dp(2),
+        shortfallUSD.dp(2),
+        accounts[currentAccount].health.dp(2),
+      );
+      findings.push(newFinding);
+    }
+
+    xxx
+  }
+
   beforeEach(async () => {
-    initializeData = {};
+    data = {};
+
     // initialize the handler
-    await (provideInitialize(initializeData))();
-    handleTransaction = provideHandleTransaction(initializeData);
-    mockERC20Contract.balanceOf.mockReset();
+    // axois mocking wasn't working as anticipated, so I'll set most of the settings here.
+    data.protocolName = config.protocolName;
+    data.protocolAbbreviation = config.protocolAbbreviation;
+    data.developerAbbreviation = config.developerAbbreviation;
+    data.alert = config.liquidationMonitor.alert;
+    data.minimumLiquidationInUSD = config.liquidationMonitor.triggerLevels.minimumLiquidationInUSD;
+    data.lowHealthThreshold = config.liquidationMonitor.triggerLevels.lowHealthThreshold;
+    data.cTokenABI = getAbi('cErc20.json');
+    data.provider = getEthersProvider();
+    data.accounts = { '0x1111': { health: 1 } }; // Health of all accounts, [assetsIn addresses]
+    data.supply = { '0xETH': { '0x1111': 1 } }; // qty of cTokens (not Tokens)
+    data.borrow = { '0xETH': { '0x1111': 1 } }; // qty of Tokens (not cTokens)
+    data.tokens = {
+      '0xETH': { price: mockEthPrice },
+      '0xBTC': { price: mockBtcPrice },
+      '0xUSDC': { price: mockUsdcPrice },
+    }; // each cToken's address, symbol, contract, ratio, price, lastUpdatePrice
+    data.newAccounts = []; // New account from transaction events
+    data.totalNewAccounts = 0;
+
+    // Compound API filter and Comptroller contract
+    const {
+      comptrollerAddress, oneInchAddress,
+    } = config.liquidationMonitor;
+    const comptrollerABI = getAbi(config.liquidationMonitor.comptrollerABI);
+    const oneInchABI = getAbi(config.liquidationMonitor.oneInchABI);
+    data.comptrollerAddress = comptrollerAddress;
+    data.comptrollerContract = new ethers.Contract(
+      comptrollerAddress,
+      comptrollerABI,
+      data.provider,
+    );
+    data.oneInchContract = new ethers.Contract(
+      oneInchAddress,
+      oneInchABI,
+      data.provider,
+    );
   });
 
-  it('returns no findings if no borrow event was emitted and no interaction with the cCOMP token occurred', async () => {
+  it('returns no findings if all tracked account healths are greater than 1', async () => {
     const mockReceipt = {
       logs: [],
     };
