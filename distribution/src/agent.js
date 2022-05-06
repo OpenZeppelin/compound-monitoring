740a437dfd6e3d8b38902e7f553e86bcff63ff98
CodeBeholder <codebeholder@arbitraryexecution.com>
2022-05-06 13:06:06 -0500

    Initial POC

--- distribution/src/agent.js
+++ distribution/src/agent.js
@@ -14,7 +14,36 @@ const ERC20_TRANSFER_EVENT = "event Transfer(address indexed from, address index
 const initializeData = {};
 
 // helper function to create distribution alerts
-function createDistributionAlert(
+function createExceedsMaximumDistributionAlert(
+  protocolName,
+  protocolAbbreviation,
+  developerAbbreviation,
+  maxCompDistribution,
+  compToken,
+  maliciousAddress,
+  compDelta,
+  compIndex,
+) {
+  const finding = Finding.fromObject({
+    name: `${protocolName} Exceeds Maximum Distribution Event`,
+    description: `Distribution of ${compDelta} COMP to ${maliciousAddress} exceeds ${maxCompDistribution}`,
+    alertId: `${developerAbbreviation}-${protocolAbbreviation}-EXCEEDS-MAXIMUM-DISTRIBUTION-EVENT`,
+    protocol: protocolName,
+    type: FindingType.Info,
+    severity: FindingSeverity.Info,
+    addresses: [
+      compToken,
+      maliciousAddress,
+    ],
+    metadata: {
+      compDelta,
+      compIndex,
+    },
+  });
+  return finding;
+}
+
+function createExceedsSaneDistributionAlert(
   protocolName,
   protocolAbbreviation,
   developerAbbreviation,
@@ -79,7 +108,7 @@ function provideInitialize(data) {
     const compContract = new ethers.Contract(compAddress, decimalsAbi, data.provider);
     const compDecimals = await compContract.decimals();
 
-    data.compDecimalsMultiplier = new BigNumber(10).pow(compDecimals);
+    data.compoundTokenDecimalsMultiplier = new BigNumber(10).pow(compDecimals);
     data.compAddress = compAddress;
   }
 }
@@ -87,15 +116,12 @@ function provideInitialize(data) {
 function provideHandleTransaction(data) {
   return async function handleTransaction(txEvent) {
     const {
-      compAddress,
-      compDecimalsMultiplier,
+      compoundTokenDecimalsMultiplier,
       comptrollerContract,
       comptrollerAddress,
       protocolName,
       protocolAbbreviation,
       developerAbbreviation,
-      distributionThresholdPercent,
-      minimumDistributionAmount,
       distributionSignatures
     } = data;
 
@@ -103,47 +129,79 @@ function provideHandleTransaction(data) {
     const findings = [];
   
     // if no events found for distributing COMP, return
-    const compDistributedEvents = txEvent.filterLog(
+    const distributionEvents = txEvent.filterLog(
       distributionSignatures, 
       comptrollerAddress
     );
 
-    if (!compDistributedEvents.length) return findings;
-    // determine how much COMP distributed using Transfer event
-    const transferEvents = txEvent.filterLog(ERC20_TRANSFER_EVENT, compAddress).filter((transferEvent) => transferEvent.args.from.toLowerCase() === comptrollerAddress);
-    for ( const transferEvent of transferEvents ) {
-      const amountCompDistributedBN = new BigNumber(transferEvent.args.value.toString())
+    await Promise.all(distributionEvents.map(async (distributionEvent) => {
+      const amountCompDistributedBN = new BigNumber(
+        distributionEvent.args.compDelta.toString(),
+      ).div(compoundTokenDecimalsMultiplier);
 
-      // if we don't reach the minimum threshold for distributed COMP do not bother calculating the ratio to the previous accrued amount
-      if ( amountCompDistributedBN.div(compDecimalsMultiplier).lt(minimumDistributionAmount) ) {
-        continue;
+      let cToken = distributionEvent.args.cToken;
+      let maliciousAddress;
+      let compIndex;
+      let compPreviousIndex;
+      let compSpeed;
+      let compState;
+      if (distributionEvent.name === 'DistributedBorrowerComp') {
+        maliciousAddress = distributionEvent.args.borrower;
+        compIndex = distributionEvent.args.compBorrowIndex;
+
+        compPreviousIndex = await comptrollerContract.compBorrowerIndex(cToken, maliciousAddress, { blockTag: txEvent.blockNumber - 1 })
+        compState = await comptrollerContract.compBorrowState(cToken, { blockTag: txEvent.blockNumber - 1 });
+        compSpeed = await comptrollerContract.compBorrowSpeeds(cToken);
+      } else if (distributionEvent.name === 'DistributedSupplierComp') {
+        maliciousAddress = distributionEvent.args.supplier;
+        compIndex = distributionEvent.args.compSupplyIndex;
+
+        compPreviousIndex = await comptrollerContract.compSupplierIndex(cToken, maliciousAddress, { blockTag: txEvent.blockNumber - 1 })
+        compState = await comptrollerContract.compSupplyState(cToken, { blockTag: txEvent.blockNumber - 1 });
+        compSpeed = await comptrollerContract.compSupplySpeeds(cToken);
       }
 
-      // determine Comptroller.compAccrued() in previous block
-      const blockNumber = txEvent.blockNumber;
-      const prevBlockCompAccrued = await comptrollerContract.compAccrued(transferEvent.args.to, { blockTag: blockNumber-1 });
-      const prevBlockCompAccruedBN = new BigNumber(prevBlockCompAccrued.toString());
-      
-      // if the previous accrual is zero, our heuristic of using the ratio will not work
-      if ( prevBlockCompAccruedBN.isZero() ) {
-        // TODO: potentially check against a maximum "sane" distribution amount of COMP and alert then even if the previous is zero?
-        continue;
-      }
+      const compSpeedBN = new BigNumber(compSpeed.toString()).div(compoundTokenDecimalsMultiplier);
+      const compIndexBN = new BigNumber(compIndex.toString()).div(compoundTokenDecimalsMultiplier);
+      const compDelta = txEvent.blockNumber - compState.block;
+      const maxCompDistribution = compSpeedBN.times(compDelta);
 
-      // calculate ratio of accrued to distributed COMP
-      const accruedToDistributedRatio = amountCompDistributedBN.div(prevBlockCompAccruedBN).times(100)
-      if (accruedToDistributedRatio.gt(distributionThresholdPercent)) {
-        findings.push(createDistributionAlert(
+      console.log("speed: ", compSpeedBN.toString());
+      console.log("block delta: ", compDelta.toString());
+      console.log("current index: ", compState.index.toString())
+      console.log("previous index: ", compPreviousIndex.toString())
+      console.log("delta index: ", )
+      console.log("max: ", maxCompDistribution.toString());
+      console.log("amount: ", amountCompDistributedBN.toString());
+
+      // check against the maximum amount of COMP that can be
+      //  distributed to a CToken
+      if (amountCompDistributedBN.gt(maxCompDistribution.div(compoundTokenDecimalsMultiplier))) {
+        findings.push(createExceedsMaximumDistributionAlert(
           protocolName,
           protocolAbbreviation,
           developerAbbreviation,
-          accruedToDistributedRatio,
-          receiver = transferEvent.args.to,
-          compDistributed = amountCompDistributedBN.toString(),
-          compAccrued = prevBlockCompAccruedBN.toString()
+          maxCompDistribution,
+          distributionEvent.args.cToken,
+          maliciousAddress,
+          amountCompDistributedBN.toString(),
+          compIndexBN.toFixed(),
+        ));
+      } else if (amountCompDistributedBN.gt(maximumSaneDistributionAmount)) { 
+      // check against a maximum "sane" distribution amount of COMP
+      //  and alert then even if the previous is zero?
+        findings.push(createExceedsSaneDistributionAlert(
+          protocolName,
+          protocolAbbreviation,
+          developerAbbreviation,
+          maximumSaneDistributionAmount,
+          distributionEvent.args.cToken,
+          maliciousAddress,
+          amountCompDistributedBN.toString(),
+          compIndexBN.toString(),
         ));
       }
-    }
+    }));
 
     return findings;
   };
@@ -154,5 +212,4 @@ module.exports = {
   initialize: provideInitialize(initializeData),
   provideHandleTransaction,
   handleTransaction: provideHandleTransaction(initializeData),
-  createDistributionAlert
 };
