5f6cdc7c05c640b424cf4a99ebcee6f596f48e39
Nomad-AE <nomad@arbitraryexecution.com>
2022-06-03 04:02:08 +0900

    AE-1406: started integrating metadata code

--- ctoken-monitor/src/autotask.js
+++ ctoken-monitor/src/autotask.js
@@ -1,37 +1,267 @@
+/* eslint-disable import/no-extraneous-dependencies,import/no-unresolved */
 const axios = require('axios');
+const ethers = require('ethers');
 
-async function post(url, method, headers, data) {
-  return axios({
-    url,
-    method,
-    headers,
-    data,
-  });
+// import the DefenderRelayProvider to interact with its JSON-RPC endpoint
+const { DefenderRelayProvider } = require('defender-relay-client/lib/ethers');
+/* eslint-enable import/no-extraneous-dependencies,import/no-unresolved */
+
+const TOKEN_ABI = [
+  'function decimals() view returns (uint8)',
+  'function symbol() view returns (string)',
+];
+const MAKER_TOKEN_ABI = [
+  'function decimals() view returns (uint256)',
+  'function symbol() view returns (bytes32)',
+];
+const CTOKEN_ABI = ['function underlying() view returns (address)'];
+
+const makerTokenAddress = '0x9f8F72aA9304c8B593d555F12eF6589cC3A579A2'.toLowerCase();
+const saiTokenAddress = '0x89d24A6b4CcB1B6fAA2625fE562bDD9a23260359'.toLowerCase();
+const oddTokens = [makerTokenAddress, saiTokenAddress];
+const cEtherAddress = '0x4Ddc2D193948926D02f9B1fE9e1daa0718270ED5'.toLowerCase();
+
+const eventMapping = {
+  Borrow: {
+    amountKey: 'borrowAmount',
+    byKey: 'borrower',
+    action: 'borrow',
+  },
+  LiquidateBorrow: {
+    amountKey: 'repayAmount',
+    byKey: 'liquidator',
+    fromKey: 'borrower',
+    action: 'liquidate',
+  },
+  Mint: {
+    amountKey: 'mintAmount',
+    byKey: 'minter',
+    action: 'supply',
+  },
+  Redeem: {
+    amountKey: 'redeemAmount',
+    byKey: 'redeemer',
+    action: 'withdraw',
+  },
+  RepayBorrow: {
+    amountKey: 'repayAmount',
+    byKey: 'payer',
+    forKey: 'borrower',
+    action: 'repay',
+  },
+};
+
+const comptrollerAddress = '0x3d9819210A31b4961b30EF54bE2aeD79B9c9Cd3B';
+const comptrollerAbi = ['function oracle() view returns (address)'];
+const oracleAbi = ['function getUnderlyingPrice(address cToken) external view returns (uint)'];
+
+async function getOracleContract(provider) {
+  // create an ethers.js Contract for the Comptroller contract
+  const comptrollerContract = new ethers.Contract(
+    comptrollerAddress,
+    comptrollerAbi,
+    provider,
+  );
+
+  // get the oracle address
+  const oracleAddress = await comptrollerContract.oracle();
+
+  // create an ethers.js Contract for the Oracle contract
+  const oracleContract = new ethers.Contract(
+    oracleAddress,
+    oracleAbi,
+    provider,
+  );
+
+  return oracleContract;
 }
 
-async function postToDiscord(url, message) {
-  const method = 'post';
+async function getTokenPrice(oracleContract, cTokenAddress, decimals) {
+  // returned price is of the form
+  //   scaledPrice = rawPrice * 1e36 / baseUnit
+  // where baseUnit is the smallest denomination of a token per whole token
+  // for example, for Ether, baseUnit = 1e18
+  const scaledPrice = await oracleContract.getUnderlyingPrice(cTokenAddress);
+
+  // calculate the baseUnit
+  const baseUnit = ethers.BigNumber.from(10).pow(decimals);
+
+  // remove the decimals scaling
+  const usdPerTokenBN = scaledPrice.mul(baseUnit);
+
+  // return the value and the number of decimals on it
+  return { usdPerTokenBN, usdPerTokenDecimals: 36 };
+}
+
+function emojiForEvent(eventName, usdValueString) {
+  // create the appropriate number of whale emoji for the value
+  // add one whale for each power of 1000
+  const numWhales = Math.floor((usdValueString.length - 1) / 3);
+  const whaleString = '🐳'.repeat(numWhales);
+
+  switch (eventName) {
+    case 'Borrow':
+      return whaleString.concat('📥');
+    case 'LiquidateBorrow':
+      return whaleString.concat('💔');
+    case 'Mint':
+      return whaleString.concat('📈');
+    case 'Redeem':
+      return whaleString.concat('📉');
+    case 'RepayBorrow':
+      return whaleString.concat('📤');
+    default:
+      return '';
+  }
+}
+
+async function getTokenInfo(cTokenAddress, provider) {
+  let underlyingTokenAddress;
+  if (cTokenAddress.toLowerCase() === cEtherAddress) {
+    underlyingTokenAddress = '0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2';
+  } else {
+    const cTokenContract = new ethers.Contract(
+      cTokenAddress,
+      CTOKEN_ABI,
+      provider,
+    );
+    underlyingTokenAddress = await cTokenContract.underlying();
+  }
+
+  let decimals;
+  let symbol;
+  if (oddTokens.indexOf(underlyingTokenAddress.toLowerCase()) !== -1) {
+    const underlyingTokenContract = new ethers.Contract(
+      underlyingTokenAddress,
+      MAKER_TOKEN_ABI,
+      provider,
+    );
+
+    decimals = await underlyingTokenContract.decimals();
+    // need to convert decimals from uint256 to uint8
+    decimals = parseInt(decimals.toString(), 10);
+
+    symbol = await underlyingTokenContract.symbol();
+    // need to convert symbol from bytes32 to string
+    symbol = ethers.utils.parseBytes32String(symbol);
+  } else {
+    const underlyingTokenContract = new ethers.Contract(
+      underlyingTokenAddress,
+      TOKEN_ABI,
+      provider,
+    );
+    decimals = await underlyingTokenContract.decimals();
+    symbol = await underlyingTokenContract.symbol();
+  }
+  return { decimals, symbol, underlyingTokenAddress };
+}
+
+function floorBigNumberString(valueString) {
+  // remove any decimal portion of the value
+  const index = valueString.indexOf('.');
+  if (index !== -1) {
+    return valueString.slice(0, index);
+  }
+  return valueString;
+}
+
+function formatAmountString(amount, decimals, usdPerTokenBN, usdPerTokenDecimals) {
+  const amountBN = ethers.BigNumber.from(amount);
+  const divisorBN = ethers.BigNumber.from(10).pow(decimals);
+  const result = amountBN.div(divisorBN);
+
+  let resultString = amountBN.toString();
+  if (resultString.length <= decimals) {
+    resultString = `0.${'0'.repeat(decimals - resultString.length)}${resultString[0]}`;
+  } else {
+    resultString = floorBigNumberString(result.toString());
+  }
+
+  const usdValueDivisor = ethers.BigNumber.from(10).pow(usdPerTokenDecimals);
+  const usdValue = usdPerTokenBN.mul(result).div(usdValueDivisor);
+  const usdValueString = floorBigNumberString(usdValue.toString());
+
+  // format the number to have comma separators for powers of 1000
+  const internationalNumberFormat = new Intl.NumberFormat('en-US');
+  return {
+    amountString: internationalNumberFormat.format(resultString),
+    usdValueString,
+  };
+}
+
+function createDiscordMessage(
+  eventName,
+  params,
+  decimals,
+  symbol,
+  usdPerTokenBN,
+  usdPerTokenDecimals,
+) {
+  const eventObject = eventMapping[eventName];
+  if (eventObject !== undefined) {
+    const amount = params[eventObject.amountKey];
+    const {
+      amountString,
+      usdValueString,
+    } = formatAmountString(amount, decimals, usdPerTokenBN, usdPerTokenDecimals);
+
+    const eventEmoji = emojiForEvent(eventName, usdValueString);
+
+    const byAddress = params[eventObject.byKey];
+    const { action } = eventObject;
+    let message = `${eventEmoji} **${amountString} ${symbol}** ${action}`;
+
+    if (action === 'liquidate') {
+      const fromAddress = params[eventObject.fromKey];
+      message += ` from ${fromAddress.slice(0, 6)} by ${byAddress.slice(0, 6)}`;
+    } else if (action === 'repayBorrow') {
+      const forAddress = params[eventObject.forKey];
+      message += ` by ${byAddress.slice(0, 6)}`;
+      if (forAddress !== byAddress) {
+        message += ` for ${forAddress.slice(0, 6)}`;
+      }
+    } else {
+      message += ` by ${byAddress.slice(0, 6)}`;
+    }
+    return message;
+  }
+  return undefined;
+}
+
+function getRandomInt(min, max) {
+  return Math.floor((Math.random() * (max - min)) + min);
+}
+
+async function postToDiscord(discordWebhook, message) {
   const headers = {
     'Content-Type': 'application/json',
   };
-  const data = JSON.stringify({ content: message });
 
+  const body = {
+    content: message,
+  };
+
+  const discordObject = {
+    url: discordWebhook,
+    method: 'post',
+    headers,
+    data: body,
+  };
   let response;
   try {
     // perform the POST request
-    response = await post(url, method, headers, data);
-  } catch (error) {
-    // is this a "too many requests" error (HTTP status 429)
-    if (error.response && error.response.status === 429) {
-      // the request was made and a response was received
-      // try again after waiting 5 seconds
+    response = await axios(discordObject);
+  } catch (err) {
+    if (err.response && err.response.status === 429) {
+      // rate-limited, retry
+      // after waiting a random amount of time between 2 and 120 seconds
+      const delay = getRandomInt(2000, 120000);
       // eslint-disable-next-line no-promise-executor-return
-      const promise = new Promise((resolve) => setTimeout(resolve, 5000));
+      const promise = new Promise((resolve) => setTimeout(resolve, delay));
       await promise;
-      response = await post(url, method, headers, data);
+      response = await axios(discordObject);
     } else {
-      // re-throw the error if it's not from a 429 status
-      throw error;
+      throw err;
     }
   }
   return response;
@@ -50,7 +280,7 @@ exports.handler = async function (autotaskEvent) {
   }
 
   // ensure that there is a DiscordUrl secret
-  const { discordUrl } = secrets;
+  const { DiscordUrl: discordUrl } = secrets;
   if (discordUrl === undefined) {
     throw new Error('discordUrl undefined');
   }
@@ -73,39 +303,85 @@ exports.handler = async function (autotaskEvent) {
     throw new Error('alert undefined');
   }
 
-  // ensure that the alert key exists within the body Object
+  // ensure that the source key exists within the body Object
   const { source } = body;
   if (source === undefined) {
     throw new Error('source undefined');
   }
 
   // extract the metadata from the alert Object
-  const {
-    metadata,
-  } = alert;
+  const { metadata } = alert;
+  if (metadata === undefined) {
+    throw new Error('metadata undefined');
+  }
 
   // extract the hashes from the source Object
   const {
-    // transactionHash,
+    transactionHash,
     block: {
-      hash,
+      hash: blockHash,
     },
   } = source;
 
   const {
-    borrowerAddress,
-    liquidationAmount,
+    cTokenSymbol,
+    contractAddress,
+    eventName,
+    ...eventArgs
   } = metadata;
 
-  const message = `📉💵🔥 **Liquidatable account detected** account ${borrowerAddress.slice(0, 6)} `
-    + `is liquidatable for $${liquidationAmount}`;
+  // use the relayer provider for JSON-RPC requests
+  const provider = new DefenderRelayProvider(autotaskEvent);
+
+  // create an ethers.js Contract for the Compound Oracle contract
+  const oracleContract = await getOracleContract(provider);
+
+  // create messages for Discord
+
+  // if there are multiple events in the transaction
+  // from multiple addresses, we won't know which event was emitted
+  // from which address, so we have to go back through all of the logs
+  // to make that determination
+  const cTokenAddress = contractAddress;
+
+  const {
+    decimals,
+    symbol,
+  } = await getTokenInfo(cTokenAddress, provider);
+
+  // get the conversion rate for this token to USD
+  const {
+    usdPerTokenBN,
+    usdPerTokenDecimals,
+  } = await getTokenPrice(oracleContract, cTokenAddress, decimals);
+
+  // craft the Discord message
+  const messages = createDiscordMessage(
+    eventName,
+    params,
+    decimals,
+    symbol,
+    usdPerTokenBN,
+    usdPerTokenDecimals,
+  );
 
   // construct the Etherscan transaction link
-  // const etherscanLink = `[TX](<https://etherscan.io/tx/${transactionHash}>)`;
-  let etherscanLink = `[BLOCK](<https://etherscan.io/block/${hash}>)`;
-  etherscanLink += ` - [ACCT](<https://etherscan.io/address/${borrowerAddress}>)`;
-  // create promises for posting messages to Discord webhook
-  await postToDiscord(discordUrl, `${etherscanLink} ${message}`);
+  const etherscanLink = `[TX](<https://etherscan.io/tx/${transactionHash}>)`;
+
+  const discordPromises = messages.map((message) => {
+    console.log(`${etherscanLink} ${message}`);
+    return postToDiscord(discordUrl, `${etherscanLink} ${message}`);
+  });
+
+  // wait for the promises to settle
+  // we want to have as many succeed as possible, so we are using
+  // .allSettled() rather than .all() here
+  let results = await Promise.allSettled(discordPromises);
+
+  results = results.filter((result) => result.status === 'rejected');
+  if (results.length > 0) {
+    throw new Error(results[0].reason);
+  }
 
   return {};
 };
--- ctoken-monitor/src/autotask.spec.js
+++ ctoken-monitor/src/autotask.spec.js
@@ -3,32 +3,17 @@ const {
 } = require('forta-agent');
 
 // Mock the data from the Bot finding
-const mockMetadata = {
-
-  metadata: {
-    cTokenSymbol: 'cETH'
-    contractAddress: '0x4Ddc2D193948926D02f9B1fE9e1daa0718270ED5',
-    eventName: 'Borrow'
-    ...eventArgs,
-  },
-
-  borrowerAddress: '0x0000000000000000000000000000000000000000',
-  liquidationAmount: '1000.00',
-  shortfallAmount: '1000.00',
-  healthFactor: '0.80',
+const mockRedeemMeta = {
+  cTokenSymbol: "cDAI",
+  contractAddress: "0x5d3a536e4d6dbd6114cc1ead35777bab948e3643",
+  eventName: "Redeem",
+  redeemAmount: "229055296840622222476",
+  redeemTokens: "1040572823132",
+  redeemer: "0xb65Ca07fD529f891A14d5Df72CCCD915A59AafF9"
 };
+
 const mockBlockHash = '0x1110890564dbd87ca848b7107487ae5a7d28da1b16707bccd3ba37381ae33419';
 const mockTxHash = '0xb85bbcdfd06edf6bcaa3271e49a339cc878daa30ec5f987a43a4d11d925ba751';
-
-const mockFinding = Finding.fromObject({
-  name: 'Placeholder Alert',
-  description: 'Placeholder description',
-  alertId: 'AE-ALERT-ID',
-  type: FindingType.Info,
-  severity: FindingSeverity.Info,
-  protocol: 'Protocol',
-  metadata: mockMetadata,
-});
 
 // grab the existing keys before loading new content from the .env file
 const existingKeys = Object.keys(process.env);
@@ -43,6 +28,19 @@ newKeys.forEach((key) => {
 });
 
 const { handler } = require('./autotask');
+
+
+function createFinding(metadata) {
+  return Finding.fromObject({
+    name: 'Placeholder Alert',
+    description: 'Placeholder description',
+    alertId: 'AE-ALERT-ID',
+    type: FindingType.Info,
+    severity: FindingSeverity.Info,
+    protocol: 'Protocol',
+    metadata,
+  });
+}
 
 function createFortaSentinelEvent(finding, blockHash, tryTxHash) {
   // Generally findings go from the Bot, to Scan Node, to Sentinel, to Autotasks
@@ -112,9 +110,19 @@ function createFortaSentinelEvent(finding, blockHash, tryTxHash) {
 
 describe('check autotask', () => {
   it('Runs autotask against mock data and posts in Discord (manual-check)', async () => {
+    const mockFinding = createFinding(mockRedeemMeta);
     const autotaskEvent = createFortaSentinelEvent(mockFinding, mockBlockHash, mockTxHash);
 
     // run the autotask on the events
     await handler(autotaskEvent);
+  });
+
+  it('throws error if discordUrl is not valid', async () => {
+    // Use an invalid discord URL
+    secrets.discordUrl = 'http//zzzz';
+    const autotaskEvent = createFortaSentinelEvent(mockFinding, mockBlockHash, mockTxHash);
+
+    // run the autotask on the events
+    await expect(handler(autotaskEvent)).rejects.toThrow('discordUrl is not a valid URL');
   });
 });
