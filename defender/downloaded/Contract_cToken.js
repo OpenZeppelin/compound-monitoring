24adb4c94ac63cb91c8e64abc20f4a8671b90d21
echoAE <89926661+echoAE@users.noreply.github.com>
2022-06-03 08:14:24 -0400

    AE-1359: Second backup of Defender account (#22)

--- defender/downloaded/Contract_cToken.js
+++ defender/downloaded/Contract_cToken.js
@@ -16,10 +16,10 @@ const MAKER_TOKEN_ABI = [
 ];
 const CTOKEN_ABI = ['function underlying() view returns (address)'];
 
-const makerTokenAddress = '0x9f8F72aA9304c8B593d555F12eF6589cC3A579A2';
-const saiTokenAddress = '0x89d24A6b4CcB1B6fAA2625fE562bDD9a23260359';
+const makerTokenAddress = '0x9f8F72aA9304c8B593d555F12eF6589cC3A579A2'.toLowerCase();
+const saiTokenAddress = '0x89d24A6b4CcB1B6fAA2625fE562bDD9a23260359'.toLowerCase();
 const oddTokens = [makerTokenAddress, saiTokenAddress];
-const cEtherAddress = '0x4Ddc2D193948926D02f9B1fE9e1daa0718270ED5';
+const cEtherAddress = '0x4Ddc2D193948926D02f9B1fE9e1daa0718270ED5'.toLowerCase();
 
 const eventMapping = {
   Borrow: {
@@ -51,6 +51,48 @@ const eventMapping = {
   },
 };
 
+const comptrollerAddress = '0x3d9819210A31b4961b30EF54bE2aeD79B9c9Cd3B';
+const comptrollerAbi = ['function oracle() view returns (address)'];
+const oracleAbi = ['function getUnderlyingPrice(address cToken) external view returns (uint)'];
+
+async function getOracleContract(provider) {
+  // create an ethers.js Contract for the Comptroller contract
+  const comptrollerContract = new ethers.Contract(
+    comptrollerAddress,
+    comptrollerAbi,
+    provider,
+  );
+
+  // get the oracle address
+  const oracleAddress = await comptrollerContract.oracle();
+
+  // create an ethers.js Contract for the Oracle contract
+  const oracleContract = new ethers.Contract(
+    oracleAddress,
+    oracleAbi,
+    provider,
+  );
+
+  return oracleContract;
+}
+
+async function getTokenPrice(oracleContract, cTokenAddress, decimals) {
+  // returned price is of the form
+  //   scaledPrice = rawPrice * 1e36 / baseUnit
+  // where baseUnit is the smallest denomination of a token per whole token
+  // for example, for Ether, baseUnit = 1e18
+  const scaledPrice = await oracleContract.getUnderlyingPrice(cTokenAddress);
+
+  // calculate the baseUnit
+  const baseUnit = ethers.BigNumber.from(10).pow(decimals);
+
+  // remove the decimals scaling
+  const usdPerTokenBN = scaledPrice.mul(baseUnit);
+
+  // return the value and the number of decimals on it
+  return { usdPerTokenBN, usdPerTokenDecimals: 36 };
+}
+
 function emojiForEvent(eventName, usdValueString) {
   // create the appropriate number of whale emoji for the value
   // add one whale for each power of 1000
@@ -73,57 +115,9 @@ function emojiForEvent(eventName, usdValueString) {
   }
 }
 
-async function getCoinGeckoData(url) {
-  let response;
-  try {
-    response = await axios.get(url);
-  } catch (err) {
-    if (err.response && err.response.status === 403) {
-      // the request was made and a response was received
-      // try again after waiting 15 seconds
-      // eslint-disable-next-line no-promise-executor-return
-      const promise = new Promise((resolve) => setTimeout(resolve, 15000));
-      await promise;
-      response = await axios.get(url);
-    } else {
-      throw err;
-    }
-  }
-  return response;
-}
-
-async function getTokenPrice(tokenAddress) {
-  const coingeckoApiUrl = 'https://api.coingecko.com/api/v3/simple/token_price/ethereum?';
-  const addressQuery = `contract_addresses=${tokenAddress}`;
-  const vsCurrency = '&vs_currencies=usd';
-
-  // create the URL
-  const url = coingeckoApiUrl.concat(addressQuery.concat(vsCurrency));
-
-  // get the price from the CoinGecko API
-  const { data } = await getCoinGeckoData(url);
-
-  // parse the response and convert the prices to ethers.js BigNumber type
-  const usdPerToken = (data[tokenAddress.toLowerCase()].usd).toString();
-
-  let usdPerTokenDecimals = 0;
-  let usdPerTokenBN;
-  const index = usdPerToken.indexOf('.');
-  if (index !== -1) {
-    const valueInt = usdPerToken.slice(0, index);
-    const valueDec = usdPerToken.slice(index + 1);
-    usdPerTokenBN = ethers.BigNumber.from(valueInt + valueDec);
-    usdPerTokenDecimals = valueDec.length;
-  } else {
-    usdPerTokenBN = ethers.BigNumber.from(usdPerToken);
-  }
-
-  return { usdPerTokenBN, usdPerTokenDecimals };
-}
-
 async function getTokenInfo(cTokenAddress, provider) {
   let underlyingTokenAddress;
-  if (cTokenAddress === cEtherAddress) {
+  if (cTokenAddress.toLowerCase() === cEtherAddress) {
     underlyingTokenAddress = '0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2';
   } else {
     const cTokenContract = new ethers.Contract(
@@ -136,7 +130,7 @@ async function getTokenInfo(cTokenAddress, provider) {
 
   let decimals;
   let symbol;
-  if (oddTokens.indexOf(underlyingTokenAddress) !== -1) {
+  if (oddTokens.includes(underlyingTokenAddress.toLowerCase())) {
     const underlyingTokenContract = new ethers.Contract(
       underlyingTokenAddress,
       MAKER_TOKEN_ABI,
@@ -234,6 +228,10 @@ function createDiscordMessage(
   return undefined;
 }
 
+function getRandomInt(min, max) {
+  return Math.floor((Math.random() * (max - min)) + min);
+}
+
 async function postToDiscord(discordWebhook, message) {
   const headers = {
     'Content-Type': 'application/json',
@@ -243,80 +241,156 @@ async function postToDiscord(discordWebhook, message) {
     content: message,
   };
 
-  // perform the POST request
-  const response = await axios({
+  const discordObject = {
     url: discordWebhook,
     method: 'post',
     headers,
-    data: JSON.stringify(body),
-  });
-
+    data: body,
+  };
+  let response;
+  try {
+    // perform the POST request
+    response = await axios(discordObject);
+  } catch (err) {
+    if (err.response && err.response.status === 429) {
+      // rate-limited, retry
+      // after waiting a random amount of time between 2 and 120 seconds
+      const delay = getRandomInt(2000, 120000);
+      // eslint-disable-next-line no-promise-executor-return
+      const promise = new Promise((resolve) => setTimeout(resolve, delay));
+      await promise;
+      response = await axios(discordObject);
+    } else {
+      throw err;
+    }
+  }
   return response;
+}
+
+function getAddressForMatchReason(reason, logs, abi) {
+  let found;
+  let parsedLog;
+  const { params, signature } = reason;
+  const iface = new ethers.utils.Interface(abi);
+  const keys = Object.keys(params);
+  let key;
+  let value;
+  for (let i = 0; i < logs.length; i++) {
+    try {
+      // attempt to parse the log
+      // because there could be many logs emitted by many contracts
+      // we don't know whether the call to parseLog will fail, hence
+      // wrapping it with a try...catch
+      // if parseLog throws, we will move on to the next log
+      parsedLog = iface.parseLog(logs[i]);
+
+      // check that the event signature matches
+      // this should be equivalent to checking the topic hashes
+      if (parsedLog.signature !== signature) {
+        // eslint-disable-next-line no-continue
+        continue;
+      }
+
+      // check that all of the parameter names and values match
+      // the keys and values in the params variable are the named
+      // arguments and their associated values for the event
+      // the parsedLog Object will have its named arguments and values
+      // stored under the args key
+      found = true;
+      for (let j = 0; j < keys.length; j++) {
+        key = keys[j];
+        value = params[key];
+        if (parsedLog.args[key].toString() !== value.toString()) {
+          found = false;
+          break;
+        }
+      }
+
+      // if a match was found, return the address from the log
+      if (found === true) {
+        return logs[i].address;
+      }
+      // eslint-disable-next-line no-empty
+    } catch {}
+  }
+  return undefined;
 }
 
 // eslint-disable-next-line func-names
 exports.handler = async function (autotaskEvent) {
-  console.log(JSON.stringify(autotaskEvent, null, 2));
-
   // ensure that the autotaskEvent Object exists
   if (autotaskEvent === undefined) {
-    return {};
+    throw new Error('autotaskEvent undefined');
   }
 
   const { secrets } = autotaskEvent;
   if (secrets === undefined) {
-    return {};
+    throw new Error('secrets undefined');
   }
 
   // ensure that there is a DiscordUrl secret
   const { DiscordUrl: discordUrl } = secrets;
   if (discordUrl === undefined) {
-    return {};
+    throw new Error('discordUrl undefined');
   }
 
   // ensure that the request key exists within the autotaskEvent Object
   const { request } = autotaskEvent;
   if (request === undefined) {
-    return {};
+    throw new Error('request undefined');
   }
 
   // ensure that the body key exists within the request Object
   const { body } = request;
   if (body === undefined) {
-    return {};
+    throw new Error('body undefined');
   }
 
   // ensure that the alert key exists within the body Object
   const {
     matchReasons,
     hash: transactionHash,
-    matchedAddresses,
+    transaction: {
+      logs,
+    },
+    sentinel: {
+      abi,
+    },
   } = body;
   if (matchReasons === undefined) {
-    return {};
+    throw new Error('matchReasons undefined');
   }
 
   // use the relayer provider for JSON-RPC requests
   const provider = new DefenderRelayProvider(autotaskEvent);
 
-  const contractAddress = ethers.utils.getAddress(matchedAddresses[0]);
+  // create an ethers.js Contract for the Compound Oracle contract
+  const oracleContract = await getOracleContract(provider);
 
   // create messages for Discord
   const promises = matchReasons.map(async (reason) => {
+    // if there are multiple events in the transaction
+    // from multiple addresses, we won't know which event was emitted
+    // from which address, so we have to go back through all of the logs
+    // to make that determination
+    const cTokenAddress = getAddressForMatchReason(reason, logs, abi);
+    if (cTokenAddress === undefined) {
+      throw new Error('unable to get address for match reason');
+    }
+
     // determine the type of event it was
     const { signature, params } = reason;
     const eventName = signature.slice(0, signature.indexOf('('));
     const {
       decimals,
       symbol,
-      underlyingTokenAddress,
-    } = await getTokenInfo(contractAddress, provider);
+    } = await getTokenInfo(cTokenAddress, provider);
 
     // get the conversion rate for this token to USD
     const {
       usdPerTokenBN,
       usdPerTokenDecimals,
-    } = await getTokenPrice(underlyingTokenAddress);
+    } = await getTokenPrice(oracleContract, cTokenAddress, decimals);
 
     // craft the Discord message
     return createDiscordMessage(
@@ -335,12 +409,20 @@ exports.handler = async function (autotaskEvent) {
   // construct the Etherscan transaction link
   const etherscanLink = `[TX](<https://etherscan.io/tx/${transactionHash}>)`;
 
-  const discordPromises = messages.map((message) => postToDiscord(discordUrl, `${etherscanLink} ${message}`));
+  const discordPromises = messages.map((message) => {
+    console.log(`${etherscanLink} ${message}`);
+    return postToDiscord(discordUrl, `${etherscanLink} ${message}`);
+  });
 
   // wait for the promises to settle
-  await Promise.all(discordPromises);
+  // we want to have as many succeed as possible, so we are using
+  // .allSettled() rather than .all() here
+  let results = await Promise.allSettled(discordPromises);
 
-  console.log('Messages sent to Discord webhook');
+  results = results.filter((result) => result.status === 'rejected');
+  if (results.length > 0) {
+    throw new Error(results[0].reason);
+  }
 
   return {};
 };
---
+++ defender/downloaded/Forta_Distributions.js
@@ -0,0 +1,220 @@
+/* eslint-disable import/no-unresolved,import/no-extraneous-dependencies */
+const ethers = require('ethers');
+const axios = require('axios');
+/* eslint-enable import/no-unresolved,import/no-extraneous-dependencies */
+
+const fortaApiEndpoint = 'https://api.forta.network/graphql';
+
+// axios post request for forta graphql api
+async function post(url, method, headers, data) {
+  return axios({
+    url, method, headers, data,
+  });
+}
+
+function createDiscordMessage(compAccrued, compDistributed, receiver, transactionHash) {
+  const receiverFormatted = receiver.slice(0, 6);
+
+  // // construct the Etherscan transaction link
+  const etherscanLink = `[TX](<https://etherscan.io/tx/${transactionHash}>)`;
+
+  // handle case for infinity % (no comp accrued before)
+  if (compAccrued.eq(0) || compDistributed.eq(0)) {
+    return `${etherscanLink} 🌊 **${receiverFormatted}** previously had no **COMP** accrued and was just distributed **COMP** tokens`;
+  }
+
+  // % disctribution % is calculated by compDistributed / compAccrued * 100
+  // format the number to have comma separators for powers of 1000
+  const internationalNumberFormat = new Intl.NumberFormat('en-US');
+
+  let percentageMore = compDistributed.div(compAccrued).mul(100).toString();
+  percentageMore = internationalNumberFormat.format(percentageMore);
+
+  return `${etherscanLink} 🌊 **${percentageMore}%** more **COMP** distributed to **${receiverFormatted}** than expected`;
+}
+
+// post to discord
+async function postToDiscord(url, message) {
+  const method = 'post';
+  const headers = {
+    'Content-Type': 'application/json',
+  };
+  const data = { content: message };
+
+  let response;
+  try {
+    // perform the POST request
+    response = await post(url, method, headers, data);
+  } catch (error) {
+    // is this a "too many requests" error (HTTP status 429)
+    if (error.response && error.response.status === 429) {
+      // the request was made and a response was received
+      // try again after waiting 5 seconds
+      // eslint-disable-next-line no-promise-executor-return
+      const promise = new Promise((resolve) => setTimeout(resolve, 5000));
+      await promise;
+      response = await post(url, method, headers, data);
+    } else {
+      // re-throw the error if it's not from a 429 status
+      throw error;
+    }
+  }
+
+  return response;
+}
+
+async function getFortaAlerts(botId, transactionHash) {
+  const headers = {
+    'content-type': 'application/json',
+  };
+
+  // construct query with data that you want to get back
+  const graphqlQuery = {
+    operationName: 'recentAlerts',
+    query: `query recentAlerts($input: AlertsInput) {
+      alerts(input: $input) {
+        pageInfo {
+          hasNextPage
+          endCursor {
+            alertId
+            blockNumber
+          }
+        }
+        alerts {
+          createdAt
+          name
+          protocol
+          findingType
+          hash
+          source {
+            transactionHash
+            block {
+              number
+              chainId
+            }
+            bot {
+              id
+            }
+          }
+          severity
+      metadata
+      description
+        }
+      }
+    }`,
+    variables: {
+      input: {
+        first: 100,
+        bots: [botId],
+        transactionHash,
+        createdSince: 0,
+        chainId: 1,
+      },
+    },
+  };
+
+  // perform the POST request
+  const response = await axios({
+    url: fortaApiEndpoint,
+    method: 'post',
+    headers,
+    data: graphqlQuery,
+  });
+
+  const { data } = response;
+  if (data === undefined) {
+    return undefined;
+  }
+
+  const { data: { alerts: { alerts } } } = data;
+  return alerts;
+}
+
+// entry point for autotask
+// eslint-disable-next-line func-names
+exports.handler = async function (autotaskEvent) {
+  // ensure that the autotaskEvent Object exists
+  if (autotaskEvent === undefined) {
+    throw new Error('autotaskEvent undefined');
+  }
+
+  const { secrets, request } = autotaskEvent;
+  if (secrets === undefined) {
+    throw new Error('secrets undefined');
+  }
+
+  // ensure that there is a DiscordUrl secret. Name changes depending on what webhook secret you use
+  const { SecurityAlertsDiscordUrl: discordUrl } = secrets;
+  if (discordUrl === undefined) {
+    throw new Error('SecurityAlertsDiscordUrl undefined');
+  }
+
+  // ensure that the request key exists within the autotaskEvent Object
+  if (request === undefined) {
+    throw new Error('request undefined');
+  }
+
+  // ensure that the body key exists within the request Object
+  const { body } = request;
+  if (body === undefined) {
+    throw new Error('body undefined');
+  }
+
+  // ensure that the alert key exists within the body Object
+  const { alert } = body;
+  if (alert === undefined) {
+    throw new Error('alert undefined');
+  }
+
+  // extract the transaction hash and bot ID from the alert Object
+  const {
+    hash,
+    source: {
+      transactionHash,
+      bot: {
+        id: botId,
+      },
+    },
+  } = alert;
+
+  // retrieve the metadata from the Forta public API
+  let alerts = await getFortaAlerts(botId, transactionHash);
+  alerts = alerts.filter((alertObject) => alertObject.hash === hash);
+
+  const promises = alerts.map((alertData) => {
+    const { metadata } = alertData;
+    let { compAccrued, compDistributed } = metadata;
+    const { receiver } = metadata;
+    compAccrued = ethers.BigNumber.from(compAccrued);
+    compDistributed = ethers.BigNumber.from(compDistributed);
+
+    return createDiscordMessage(
+      compAccrued,
+      compDistributed,
+      receiver,
+      transactionHash,
+    );
+  });
+
+  // wait for the promises to settle
+  let results = await Promise.allSettled(promises);
+
+  // construct the Etherscan transaction link
+  const etherscanLink = `[TX](<https://etherscan.io/tx/${transactionHash}>)`;
+
+  // create promises for posting messages to Discord webhook
+  const discordPromises = results.map((result) => {
+    console.log(`${etherscanLink} ${result.value}`);
+    return postToDiscord(discordUrl, `${etherscanLink} ${result.value}`);
+  });
+
+  // wait for the promises to settle
+  results = await Promise.allSettled(discordPromises);
+  results = results.filter((result) => result.status === 'rejected');
+
+  if (results.length > 0) {
+    throw new Error(results[0].reason);
+  }
+
+  return {};
+};
