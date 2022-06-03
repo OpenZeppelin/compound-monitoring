211cde3c8893889e4669f4d18e5e45b8525d640f
Nomad-AE <nomad@arbitraryexecution.com>
2022-06-03 23:46:45 +0900

    AE-1406: add liquidate test

--- ctoken-monitor/src/autotask.spec.js
+++ ctoken-monitor/src/autotask.spec.js
@@ -6,15 +6,17 @@ const {
 // Random block
 const mockBlockHash = '0x1110890564dbd87ca848b7107487ae5a7d28da1b16707bccd3ba37381ae33419';
 
-const mockRedeemTxHash = '0xa1be93b4be553650aec7c4d99dcefcad23c573c5a23004fbc7c0dfe4179d62ce';
-const mockRedeemMeta = {
-  cTokenSymbol: 'cUSDC',
-  contractAddress: '0x39AA39c021dfbaE8faC545936693aC917d5E7563',
-  eventName: 'Redeem',
-  redeemAmount: '894873',
-  redeemTokens: '20224',
-  redeemer: '0xb65Ca07fD529f891A14d5Df72CCCD915A59AafF9',
-  usdValue: '20224',
+const mockLiquidateBorrowTxHash = '0x064228d15febb05b929e8aecbf3d828449bd8210df758d692b9b855355ed3560';
+const mockLiquidateBorrowMeta = {
+  borrower: '0xf1C6A281452fEdEAda164731895B1a38b5476516',
+  cTokenCollateral: '0x4Ddc2D193948926D02f9B1fE9e1daa0718270ED5',
+  cTokenSymbol: 'cUSDT',
+  contractAddress: '0xf650c3d88d12db855b8bf7d11be6c55a4e07dcc9',
+  eventName: 'LiquidateBorrow',
+  liquidator: '0xD911560979B78821D7b045C79E36E9CbfC2F6C6F',
+  repayAmount: '880985666',
+  seizeTokens: '2710480386',
+  usdValue: '881',
 };
 
 const mockMintTxHash = '0xff85476c183ef3cc0fb0623877abf5589197a773845f8acac341e48c42957a3e';
@@ -26,6 +28,30 @@ const mockMintMeta = {
   mintTokens: '190925893578',
   minter: '0x352E490bC98BB07AA908Cc2934b6Ca473a6b229d',
   usdValue: '67721',
+};
+
+const mockRedeemTxHash = '0xa1be93b4be553650aec7c4d99dcefcad23c573c5a23004fbc7c0dfe4179d62ce';
+const mockRedeemMeta = {
+  cTokenSymbol: 'cUSDC',
+  contractAddress: '0x39AA39c021dfbaE8faC545936693aC917d5E7563',
+  eventName: 'Redeem',
+  redeemAmount: '894873',
+  redeemTokens: '20224',
+  redeemer: '0xb65Ca07fD529f891A14d5Df72CCCD915A59AafF9',
+  usdValue: '20224',
+};
+
+const mockRepayBorrowTxHash = '0x57a36644b7440ad247a41222ad105d5a08d21b47e434025bcf4427b2c20f3dee';
+const mockRRepayBorrowMeta = {
+  accountBorrows: '1457196372779',
+  borrower: '0xF6aaadA76eA7f044ffA028565b028740Dce5389F',
+  cTokenSymbol: 'cUSDC',
+  contractAddress: '0x39aa39c021dfbae8fac545936693ac917d5e7563',
+  eventName: 'RepayBorrow',
+  payer: '0xF6aaadA76eA7f044ffA028565b028740Dce5389F',
+  repayAmount: '19985757121',
+  totalBorrows: '449947401135954',
+  usdValue: '19985',
 };
 
 // grab the existing keys before loading new content from the .env file
@@ -121,9 +147,13 @@ function createFortaSentinelEvent(finding, blockHash, tryTxHash) {
 }
 
 describe('check autotask', () => {
-  it('Runs autotask against mock Redeem data and posts in Discord (manual-check)', async () => {
-    const mockFinding = createFinding(mockRedeemMeta);
-    const autotaskEvent = createFortaSentinelEvent(mockFinding, mockBlockHash, mockRedeemTxHash);
+  it('Runs autotask against mock LiquidateBorrow data and posts in Discord (manual-check)', async () => {
+    const mockFinding = createFinding(mockLiquidateBorrowMeta);
+    const autotaskEvent = createFortaSentinelEvent(
+      mockFinding,
+      mockBlockHash,
+      mockLiquidateBorrowTxHash,
+    );
 
     // run the autotask on the events
     await handler(autotaskEvent);
@@ -132,6 +162,26 @@ describe('check autotask', () => {
   it('Runs autotask against mock Mint data and posts in Discord (manual-check)', async () => {
     const mockFinding = createFinding(mockMintMeta);
     const autotaskEvent = createFortaSentinelEvent(mockFinding, mockBlockHash, mockMintTxHash);
+
+    // run the autotask on the events
+    await handler(autotaskEvent);
+  });
+
+  it('Runs autotask against mock Redeem data and posts in Discord (manual-check)', async () => {
+    const mockFinding = createFinding(mockRedeemMeta);
+    const autotaskEvent = createFortaSentinelEvent(mockFinding, mockBlockHash, mockRedeemTxHash);
+
+    // run the autotask on the events
+    await handler(autotaskEvent);
+  });
+
+  it('Runs autotask against mock RepayBorrow data and posts in Discord (manual-check)', async () => {
+    const mockFinding = createFinding(mockRRepayBorrowMeta);
+    const autotaskEvent = createFortaSentinelEvent(
+      mockFinding,
+      mockBlockHash,
+      mockRepayBorrowTxHash,
+    );
 
     // run the autotask on the events
     await handler(autotaskEvent);
