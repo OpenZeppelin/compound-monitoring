c0005f4f4a40c81e226a217759455f9e6a323ea9
Nomad-AE <nomad@arbitraryexecution.com>
2022-06-14 15:47:55 +0900

    AE-1405: remove console logs
    Co-authored-by: darian-ae <100232770+darian-ae@users.noreply.github.com>

--- defender/downloaded/Forta_Oracle_Price.js
+++ defender/downloaded/Forta_Oracle_Price.js
@@ -265,7 +265,6 @@ exports.handler = async function (autotaskEvent) {
 
   // create promises for posting messages to Discord webhook
   const discordPromises = results.map((result) => {
-    console.log(`${result.value}`);
     return postToDiscord(discordUrl, `${result.value}`);
   });
 
