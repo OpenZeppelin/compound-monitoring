f8842628d72f12137d644e947f5acef3a67b5814
Nomad-AE <nomad@arbitraryexecution.com>
2022-06-16 18:15:22 +0900

    AE-1406: added logging
    Co-authored-by: echoAE <89926661+echoAE@users.noreply.github.com>

--- defender/staged/downloaded/Forta_Oracle_Price.js
+++ defender/staged/downloaded/Forta_Oracle_Price.js
@@ -146,6 +146,8 @@ exports.handler = async function (autotaskEvent) {
   const message = `${etherscanLink} 🚫 Reported price of **${underlyingAddressFormatted}** was **rejected**`;
 
   // create promises for posting messages to Discord webhook
+  // with Log Forwarding enabled, this console.log will forward the text string to Dune Analytics
+  console.log(message);
   await postToDiscord(discordUrl, message);
 
   return {};
