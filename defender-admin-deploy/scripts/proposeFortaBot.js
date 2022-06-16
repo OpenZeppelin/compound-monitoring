57b292300f4e34cff6b8a92fe8f843ee02902d48
darian-ae <darian@arbitraryexecution.com>
2022-06-15 20:43:22 -0700

    wrote proposal post request but getting a 400 error

--- defender-admin-deploy/scripts/proposeFortaBot.js
+++ defender-admin-deploy/scripts/proposeFortaBot.js
@@ -15,6 +15,7 @@ const {
     chainIds,
   } = container.cradle;
 
+
 // load values from the .env file
 require('dotenv').config();
 
@@ -26,7 +27,7 @@ const ownerPrivateKey = process.env.PROPOSE_PRIVATE_KEY;
 
 // load defender api keys and secrets
 const defenderApiKey = process.env.DEFENDER_API_KEY;
-const defnederApiSecret = process.env.DEFENDER_SECRET_KEY;
+const defenderApiSecret = process.env.DEFENDER_SECRET_KEY;
 
 async function main() {
   // set up an ethers.js provider
@@ -36,34 +37,65 @@ async function main() {
   const deployerWallet = new ethers.Wallet(ownerPrivateKey);
   const signer = deployerWallet.connect(provider);
 
-//   // Forta SDK function
-//   // get the private key for signing the manifest from the forta.config.json file
-//   const { privateKey } = await getCredentials();
+  // Forta SDK function
+  // get the private key for signing the manifest from the forta.config.json file
+  const { privateKey } = await getCredentials();
 
-//   // set up an ethers.js Wallet
-//   const fortaDeployerWallet = new ethers.Wallet(privateKey);
+  // set up an ethers.js Wallet
+  // const fortaDeployerWallet = new ethers.Wallet(privateKey);
 
-//   // Forta SDK function
-//   // get the ethers.js Contract Object for interacting with the Forta AgentRegistry contract
-//   const contract = agentRegistry.getContract(fortaDeployerWallet);
+  // Forta SDK function
+  // get the ethers.js Contract Object for interacting with the Forta AgentRegistry contract
+  // const contract = agentRegistry.getContract(fortaDeployerWallet);
 
-//   // Forta SDK function
-//   // uploadImage() will look for a Dockerfile in the main level of the repository
-//   // then it will invoke 'docker build' and push the image to the Forta Disco image repository
-//   const imageReference = await uploadImage();
+  // Forta SDK function
+  // uploadImage() will look for a Dockerfile in the main level of the repository
+  // then it will invoke 'docker build' and push the image to the Forta Disco image repository
+  const imageReference = await uploadImage();
 
-//   // Forta SDK function
-//   // uploadManifest() will:
-//   //   - upload documentation to IPFS
-//   //   - create a bot manifest
-//   //   - sign the bot manifest with a privateKey
-//   //   - upload the manifest and signature to IPFS
-//   //   - return the content ID for that content
-//   const manifestReference = await uploadManifest(imageReference, privateKey);
+  // Forta SDK function
+  // uploadManifest() will:
+  //   - upload documentation to IPFS
+  //   - create a bot manifest
+  //   - sign the bot manifest with a privateKey
+  //   - upload the manifest and signature to IPFS
+  //   - return the content ID for that content
+  const manifestReference = await uploadManifest(imageReference, privateKey);
 
-const client = new AdminClient({apiKey: defenderApiKey, apiSecret: defnederApiSecret});
+  const owner = signer.address // the user's address who input their private key in the .env will be the owner
 
+  const client = new AdminClient({apiKey: defenderApiKey, apiSecret: defenderApiSecret});
 
+  // function createAgent(uint256 agentId, address owner, string calldata metadata, uint256[] calldata chainIds)
+  await client.createProposal({
+    contract: { address: agentRegistry.agentRegistryContractAddress, network: 'matic'}, // target contract - should be the forta bot registrar smart contract
+    title: 'Forta Bot Deployment Test', // title of proposal
+    description: 'Testing Forta Bot deployment from Defender Admin', // description of proposal
+    type: 'custom', // put custom for custom actions (anything that's not a pause, unpause or upgrade action)
+    functionInterface: { // function ABI that you want to target
+      name: "createAgent",
+      inputs: 
+        [{
+          "name": "agentId",
+          "type": "uint256"
+        },
+        {
+          "name": "owner",
+          "type": "address"
+        },
+        {
+          "name": "metadata",
+          "type": "string"
+        },
+        {
+          "name": "chainIds",
+          "type": "uint256[]"
+        }]
+    },
+    functionInputs: [agentId, owner, manifestReference, chainIds], // function inputs
+    via: signer.address, // address you want to execute the proposal
+    viaType: 'EOA' // 'Gnosis Safe', 'Gnosis Multisig', or 'EOA'
+  })
 }
 
 main()
