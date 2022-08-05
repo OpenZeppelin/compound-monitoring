a76fca966b33913635c07924591d23b2f0168b09
Nomad-AE <nomad@arbitraryexecution.com>
2022-08-05 19:24:33 +0900

    AE-1672: switched to unresolved promises

--- liquidation-monitor-v3/src/agent.js
+++ liquidation-monitor-v3/src/agent.js
@@ -18,12 +18,17 @@ function adjustBalance(data, event) {
     asset = baseToken, src, dst, from, to, amount,
   } = event.args;
   switch (event.name) {
-    case 'Supply':
+    case 'Withdraw':
       // Initialize accounts and assets if they don't exist
-      console.debug(`User ${dst} supplied ${amount} of ${asset}`);
-      if (users[dst] === undefined) { users[dst] = {}; }
-      if (users[dst][asset] === undefined) { users[dst][asset] = ethers.BigNumber.from(0); }
-      users[dst][asset] = users[dst][asset].add(amount);
+      if (users[src] === undefined) { users[src] = {}; }
+
+      // Anytime a user withdraws the baseToken, they are marked at a possibleBorrower.
+      // From only the event, it is not possible to determine if the user is withdrawing previous
+      // collateral or borrowing assets. Therefore, they will be scanned in a later section.
+      if (users[src].possibleBorrower !== true) {
+        console.debug(`Possible borrower ${src} found`);
+        users[src].possibleBorrower = true;
+      }
       break;
     case 'SupplyCollateral':
       // Initialize accounts and assets if they don't exist
@@ -31,17 +36,6 @@ function adjustBalance(data, event) {
       if (users[dst] === undefined) { users[dst] = {}; }
       if (users[dst][asset] === undefined) { users[dst][asset] = ethers.BigNumber.from(0); }
       users[dst][asset] = users[dst][asset].add(amount);
-      break;
-    case 'Withdraw':
-      // Initialize accounts and assets if they don't exist
-      console.debug(`User ${src} withdrew ${amount} of ${asset}`);
-      if (users[src] === undefined) { users[src] = {}; }
-      if (users[src][asset] === undefined) { users[src][asset] = ethers.BigNumber.from(0); }
-      // Anytime a user withdraws the baseToken, they are marked at a possibleBorrower.
-      // It is not possible to determine if the withdraw is taking back previous collateral or
-      // borrowing assets. Therefore, they will be scanned in a later section.
-      users[src].possibleBorrower = true;
-      users[src][asset] = users[src][asset].add(amount);
       break;
     case 'WithdrawCollateral':
       // Initialize accounts and assets if they don't exist
@@ -65,6 +59,13 @@ function adjustBalance(data, event) {
   }
 }
 
+function getBorrowersList(users) {
+  const borrowers = Object.entries(users)
+    // eslint-disable-next-line no-unused-vars
+    .filter(([user, value]) => value.possibleBorrower).map(([user, value]) => user);
+  return borrowers;
+}
+
 function provideInitialize(data) {
   return async function initialize() {
     // Initialize
@@ -82,7 +83,17 @@ function provideInitialize(data) {
     data.baseToken = await cometContract.baseToken();
     data.baseScale = await cometContract.baseScale();
     data.baseTokenPriceFeed = await cometContract.baseTokenPriceFeed();
-    const { baseToken } = data;
+    data.topics = [[
+      // cometInterface.getEventTopic('Supply'), // Don't need to track this
+      // cometInterface.getEventTopic('Transfer'), // Don't need to track this
+      cometInterface.getEventTopic('Withdraw'),
+      cometInterface.getEventTopic('SupplyCollateral'),
+      cometInterface.getEventTopic('TransferCollateral'),
+      cometInterface.getEventTopic('WithdrawCollateral'),
+    ]];
+    data.numAssets = await cometContract.numAssets();
+
+    const { topics } = data;
     /* eslint-enable no-param-reassign */
 
     // Ref: https://kovan.etherscan.io/address/0xcc861650dc6f25cb5ab4185d4657a70c923fdb27#code
@@ -95,14 +106,7 @@ function provideInitialize(data) {
       fromBlock: '0x0',
       toBlock: 'latest',
       address: cometAddress,
-      topics: [[
-        cometInterface.getEventTopic('Supply'),
-        // cometInterface.getEventTopic('Transfer'), // Don't need to track this
-        cometInterface.getEventTopic('Withdraw'),
-        cometInterface.getEventTopic('SupplyCollateral'),
-        cometInterface.getEventTopic('TransferCollateral'),
-        cometInterface.getEventTopic('WithdrawCollateral'),
-      ]],
+      topics,
     });
     const parsedEvents = rawLogs.map((log) => cometInterface.parseLog(log));
 
@@ -116,6 +120,90 @@ function provideInitialize(data) {
 function provideHandleBlock(data) {
   return async function handleBlock(blockEvent) {
     // Process the block
+    const { number: blockNumber } = blockEvent.block;
+    const {
+      assets, cometContract, cometInterface, provider, topics, users,
+    } = data;
+
+    // Each await is initiated individually and references the most up to date info that it has
+    // access to. Previous promises may not be resolved by the time subsequent calls are made.
+    // Calculations may be off by up to 3 blocks (in testing). Example: user.balance information is
+    // from block 100 and the pricing data is from block 102. To account for this, any account
+    // within x% of liquidation will be checked on-chain with `isBorrowCollateralized(address)` and
+    // `isLiquidatable(address)` calls.
+
+    // Scan for events in the latest block
+    async function processLogs() {
+      const rawLogs = await provider.getLogs({
+        fromBlock: 'latest',
+        toBlock: 'latest',
+        address: cometAddress,
+        topics,
+      });
+      const parsedEvents = rawLogs.map((log) => cometInterface.parseLog(log));
+
+      // Update state of all borrowers
+      parsedEvents.forEach((event) => {
+        adjustBalance(data, event);
+      });
+      console.debug(`Finished processingLogs in block ${blockNumber}`);
+    }
+    const logsPromise = processLogs();
+
+    // Update baseToken Balances for possible borrowers
+    async function updateBalances() {
+      const borrowers = getBorrowersList(users);
+      borrowers.map(async (user) => {
+        users[user].borrowBalance = await cometContract.borrowBalanceOf(user);
+        // console.debug(`User ${user} has ${users[user].borrowBalance} debt`);
+        if (users[user].borrowBalance.eq(0)) {
+          console.debug(`User ${user} is debt free, removing them from the list`);
+          users[user].possibleBorrower = false;
+        }
+      });
+      console.debug(`Finished updateBalances in block ${blockNumber}`);
+    }
+    const balancesPromis = updateBalances();
+
+    // Update all asset info and prices
+    async function updateNumAssets() {
+    // returns uint8 as a number, not BigNumber
+      data.numAssets = await cometContract.numAssets();
+      console.debug(`Finished updateNumAssets in block ${blockNumber}`);
+    }
+    const numAssetsPromise = updateNumAssets();
+
+    //
+    async function updateAssetInfo() {
+    // Generates an array of numbers from 0 to numAssets
+      const indexes = [...Array(data.numAssets).keys()];
+      indexes.map(async (index) => {
+        const info = await cometContract.getAssetInfo(index);
+        const {
+          asset,
+          priceFeed,
+          scale,
+          borrowCollateralFactor,
+          liquidateCollateralFactor,
+          liquidationFactor,
+        } = info;
+        assets[asset] = {
+          priceFeed,
+          scale,
+          borrowCollateralFactor,
+          liquidateCollateralFactor,
+          liquidationFactor,
+        };
+      });
+      console.debug(`Finished updateAssetInfo in block ${blockNumber}`);
+    }
+    updateAssetInfo();
+
+    // price: await cometContract.getPrice(priceFeed),
+
+    // await Promise.all(, );
+    // console.debug(assets);
+    console.debug('ready');
     return [];
   };
 }
@@ -123,6 +211,7 @@ function provideHandleBlock(data) {
 function provideHandleTransaction(data) {
   return async function handleTransaction(txEvent) {
     // Process transaction
+    // Disabled for now. Should I watch every transaction or check the logs from the block?
     return [];
   };
 }
