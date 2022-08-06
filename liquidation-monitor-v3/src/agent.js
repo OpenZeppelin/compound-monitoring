a597998faa3ae8629b97b6c6be11d729b93513d9
Nomad-AE <nomad@arbitraryexecution.com>
2022-08-06 13:11:12 +0900

    AE-1672: streamlined promises

--- liquidation-monitor-v3/src/agent.js
+++ liquidation-monitor-v3/src/agent.js
@@ -4,7 +4,7 @@ const {
 } = require('forta-agent');
 const abi = require('../abi/comet.json');
 
-const { Contract } = ethers;
+const { BigNumber, Contract } = ethers;
 const { Interface } = ethers.utils;
 
 const cometAddress = '0xcC861650dc6f25cB5Ab4185d4657a70c923FDb27';
@@ -34,14 +34,14 @@ function adjustBalance(data, event) {
       // Initialize accounts and assets if they don't exist
       console.debug(`User ${dst} supplied ${amount} of ${asset}`);
       if (users[dst] === undefined) { users[dst] = {}; }
-      if (users[dst][asset] === undefined) { users[dst][asset] = ethers.BigNumber.from(0); }
+      if (users[dst][asset] === undefined) { users[dst][asset] = BigNumber.from(0); }
       users[dst][asset] = users[dst][asset].add(amount);
       break;
     case 'WithdrawCollateral':
       // Initialize accounts and assets if they don't exist
       console.debug(`User ${src} withdrew ${amount} of ${asset}`);
       if (users[src] === undefined) { users[src] = {}; }
-      if (users[src][asset] === undefined) { users[src][asset] = ethers.BigNumber.from(0); }
+      if (users[src][asset] === undefined) { users[src][asset] = BigNumber.from(0); }
       users[src][asset] = users[src][asset].add(amount);
       break;
     case 'TransferCollateral':
@@ -49,9 +49,9 @@ function adjustBalance(data, event) {
       console.debug(`User ${from} transferred out ${amount} of ${asset}`);
       console.debug(`User ${to} transferred in ${amount} of ${asset}`);
       if (users[from] === undefined) { users[from] = {}; }
-      if (users[from][asset] === undefined) { users[from][asset] = ethers.BigNumber.from(0); }
+      if (users[from][asset] === undefined) { users[from][asset] = BigNumber.from(0); }
       if (users[to] === undefined) { users[to] = {}; }
-      if (users[to][asset] === undefined) { users[to][asset] = ethers.BigNumber.from(0); }
+      if (users[to][asset] === undefined) { users[to][asset] = BigNumber.from(0); }
       users[from][asset] = users[from][asset].sub(amount);
       users[to][asset] = users[to][asset].add(amount);
       break;
@@ -66,6 +66,52 @@ function getBorrowersList(users) {
   return borrowers;
 }
 
+async function updateAssetInfo(data, blockNumber) {
+  const { assets, cometContract } = data;
+  // Generates an array of numbers from 0 to numAssets
+  const indexes = [...Array(data.numAssets).keys()];
+  await Promise.all(indexes.map(async (index) => {
+    const info = await cometContract.getAssetInfo(index);
+    const {
+      asset,
+      priceFeed,
+      scale,
+      borrowCollateralFactor,
+      liquidateCollateralFactor,
+      liquidationFactor,
+    } = info;
+    assets[asset] = {
+      price: BigNumber.from(0),
+      priceFeed,
+      scale,
+      borrowCollateralFactor,
+      liquidateCollateralFactor,
+      liquidationFactor,
+    };
+  }));
+  console.debug(`Finished updateAssetInfo in block ${blockNumber}`);
+}
+
+async function updatePrices(data, blockNumber) {
+  const { assets, cometContract } = data;
+  await Promise.all(Object.entries(assets).map(async ([asset, value]) => {
+    const price = await cometContract.getPrice(value.priceFeed);
+    assets[asset].price = price;
+    console.debug(`In block ${blockNumber} the price of ${asset} is ${price}`);
+  }));
+  console.debug(`Finished updatePrices in block ${blockNumber}`);
+}
+
+async function getLiquidatable(data, blockNumber) {
+  const { users, cometContract } = data;
+  const borrowers = getBorrowersList(users);
+
+  await Promise.all(borrowers.map(async (borrower) => {
+    const isLiquidatable = await cometContract.isLiquidatable(borrower);
+    // console.debug(`User ${borrower} isLiquidatable ${isLiquidatable}`);
+  }));
+}
+
 function provideInitialize(data) {
   return async function initialize() {
     // Initialize
@@ -76,32 +122,27 @@ function provideInitialize(data) {
     data.cometInterface = new Interface(abi);
     data.cometContract = new Contract(cometAddress, abi, getEthersProvider());
 
-    const {
-      assets, cometContract, cometInterface, provider, users,
-    } = data;
+    const { cometContract, cometInterface, provider } = data;
 
     data.baseToken = await cometContract.baseToken();
     data.baseScale = await cometContract.baseScale();
     data.baseTokenPriceFeed = await cometContract.baseTokenPriceFeed();
     data.topics = [[
-      // cometInterface.getEventTopic('Supply'), // Don't need to track this
-      // cometInterface.getEventTopic('Transfer'), // Don't need to track this
       cometInterface.getEventTopic('Withdraw'),
       cometInterface.getEventTopic('SupplyCollateral'),
       cometInterface.getEventTopic('TransferCollateral'),
       cometInterface.getEventTopic('WithdrawCollateral'),
+      cometInterface.getEventTopic('AbsorbDebt'),
+      cometInterface.getEventTopic('AbsorbCollateral'),
     ]];
     data.numAssets = await cometContract.numAssets();
-
-    const { topics } = data;
     /* eslint-enable no-param-reassign */
-
-    // Ref: https://kovan.etherscan.io/address/0xcc861650dc6f25cb5ab4185d4657a70c923fdb27#code
 
     // Get list of all user interactions
     // NOTE: For Infura and Alchemy, eth_getLogs requests are limited to 10K logs in the response.
     // ref: https://docs.infura.io/infura/networks/ethereum/json-rpc-methods/eth_getlogs#limitations
     // ref: https://docs.alchemy.com/alchemy/apis/ethereum/eth-getlogs
+    const { topics } = data;
     const rawLogs = await provider.getLogs({
       fromBlock: '0x0',
       toBlock: 'latest',
@@ -114,6 +155,12 @@ function provideInitialize(data) {
     parsedEvents.forEach((event) => {
       adjustBalance(data, event);
     });
+
+    // Update Asset priceFeed, scale, borrowCollateralFactor and liquidateCollateralFactor
+    await updateAssetInfo(data, 0);
+
+    // Update Prices
+    await updatePrices(data, 0);
   };
 }
 
@@ -148,73 +195,54 @@ function provideHandleBlock(data) {
       });
       console.debug(`Finished processingLogs in block ${blockNumber}`);
     }
-    const logsPromise = processLogs();
+    processLogs();
 
     // Update baseToken Balances for possible borrowers
     async function updateBalances() {
       const borrowers = getBorrowersList(users);
-      borrowers.map(async (user) => {
+      await Promise.all(borrowers.map(async (user) => {
         users[user].borrowBalance = await cometContract.borrowBalanceOf(user);
         // console.debug(`User ${user} has ${users[user].borrowBalance} debt`);
         if (users[user].borrowBalance.eq(0)) {
           console.debug(`User ${user} is debt free, removing them from the list`);
           users[user].possibleBorrower = false;
         }
-      });
+      }));
       console.debug(`Finished updateBalances in block ${blockNumber}`);
     }
-    const balancesPromis = updateBalances();
+    updateBalances();
 
-    // Update all asset info and prices
+    // Update the number of assets in Comet
     async function updateNumAssets() {
     // returns uint8 as a number, not BigNumber
+      // eslint-disable-next-line no-param-reassign
       data.numAssets = await cometContract.numAssets();
       console.debug(`Finished updateNumAssets in block ${blockNumber}`);
     }
-    const numAssetsPromise = updateNumAssets();
+    updateNumAssets();
 
-    //
-    async function updateAssetInfo() {
-    // Generates an array of numbers from 0 to numAssets
-      const indexes = [...Array(data.numAssets).keys()];
-      indexes.map(async (index) => {
-        const info = await cometContract.getAssetInfo(index);
-        const {
-          asset,
-          priceFeed,
-          scale,
-          borrowCollateralFactor,
-          liquidateCollateralFactor,
-          liquidationFactor,
-        } = info;
-        assets[asset] = {
-          priceFeed,
-          scale,
-          borrowCollateralFactor,
-          liquidateCollateralFactor,
-          liquidationFactor,
-        };
-      });
-      console.debug(`Finished updateAssetInfo in block ${blockNumber}`);
-    }
-    updateAssetInfo();
+    // Update Asset priceFeed, scale, borrowCollateralFactor and liquidateCollateralFactor
+    updateAssetInfo(data, blockNumber);
 
-    // price: await cometContract.getPrice(priceFeed),
+    // Update Prices
+    const pricePromise = updatePrices(data, blockNumber);
 
-    // await Promise.all(, );
-    // console.debug(assets);
-    console.debug('ready');
+    findings = getLiquidatable(data, blockNumber);
+
+    await Promise.all([findings]);
+    console.debug(assets);
+    console.debug(`End block ${blockNumber}`);
     return [];
   };
 }
 
-function provideHandleTransaction(data) {
-  return async function handleTransaction(txEvent) {
-    // Process transaction
-    // Disabled for now. Should I watch every transaction or check the logs from the block?
-    return [];
-  };
-}
+// function provideHandleTransaction(data) {
+//   return async function handleTransaction(txEvent) {
+//     // Process transaction
+//     // Disabled for now. Should I watch every transaction or check the logs from the block?
+//     return [];
+//   };
+// }
 
 module.exports = {
   provideHandleBlock,
