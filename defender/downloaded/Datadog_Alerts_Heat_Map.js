d48004b6b64648016b80385c7922a61f1738882a
echo <echo@arbitraryexecution.com>
2022-10-26 10:12:10 -0400

    AE-2052: Updating Datadog Autotasks to point at newer Forta Bot IDs and pass Bot names to Datadog Dashboard.

--- defender/downloaded/Datadog_Alerts_Heat_Map.js
+++ defender/downloaded/Datadog_Alerts_Heat_Map.js
@@ -8,13 +8,13 @@ const { KeyValueStoreClient } = require('defender-kvstore-client');
 const fortaExplorerEarliestTimestamp = 1638721490212;
 
 const botIdsToNames = {
-  '0x5a00b44b2db933d4c797e6bd3049abdeb89cc9ec1b2eaee7bdbaff911794f714': 'Low Liquidity Attack',
+  '0xe49ab07879658c258d5007ac6b88428a2b88cc5cfef206222ad94690840be87a': 'Low Liquidity Attack',
   '0xb6bdedbae67cc82e60aad02a8ffab3ccbefeaa876ca7e4f291c07c798a95e339': 'Large Borrows Governance',
-  '0x916603512086fcad84c35858d2fc5356c512f72b19c80e52e8f9c04d8122e2ba': 'Community Multi-Sig',
+  '0x2e7f036f3495fec41a3eabae03b3efd378f6834bbb2976b99dfed6d3c7de7458': 'Community Multi-Sig',
   '0x0d3cdcc2757cd7837e3b302a9889c854044a80835562dc8060d7c163fbb69d53': 'Large Delegations',
-  '0xe200d890a67d51c3610520dd9fdfa9e2bd6dd341d41e32fa457601e73c4c6685': 'Oracle Price',
-  '0xf836bda7810aa2dd9df5bb7ac748f173b945863e922a15bb7c57da7b0e6dab05': 'Underlying Asset',
-  '0x125c36816fbad9974a452947bf6a98d975988ddf4342c159a986383b64765e22': 'Market Activity',
+  '0x32facccd163300ad76c7fe88b559b812dca9050a569417b42fced95594dda08e': 'Oracle Price',
+  '0xfa3044aa08927163ff8578fb5c108978dfde3a12e0b21834e53111e2859f3a59': 'Underlying Asset',
+  '0xab39733ddf86340b8e7940ebb933bb48506a341485c7f8c77da17bc1400fe028': 'Market Activity',
   '0xa0424dfee87cc34b9ff6a1dfa2cb22dbf1b20a238698ae0eeffbf07f869e5b39': 'Governance Activity',
 };
 
@@ -115,10 +115,12 @@ function createAlertsQuery(botId, currentTimestamp, lastUpdateTimestamp) {
         }
         nextPageValues {
           blocknumber
+          timestamp
           id
         }
         currentPageValues {
           blocknumber
+          timestamp
           id
         }
       }
