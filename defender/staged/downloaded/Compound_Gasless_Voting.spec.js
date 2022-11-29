07f3b735035acd4553e38e76e8abf27354240e53
darian-ae <darian@arbitraryexecution.com>
2022-11-29 10:45:45 -0800

    added more tests

--- defender/staged/downloaded/Compound_Gasless_Voting.spec.js
+++ defender/staged/downloaded/Compound_Gasless_Voting.spec.js
@@ -19,16 +19,16 @@ jest.mock('defender-relay-client/lib/ethers', () => ({
 
 const { handler } = require('./Compound_Gasless_Voting');
 
-function createAutotaskEvent(txsBatch) {
+function createAutotaskEvent(batchedTxs) {
   const autotaskEvent = {
     request: {
-      body: txsBatch,
+      body: batchedTxs,
     },
   };
   return autotaskEvent;
 }
 
-function createVoteObject(address, proposalId, support, v, r, s) {
+function createCastVoteObject(address, proposalId, support, v, r, s) {
   const voteObject = {
     address,
     proposalId,
@@ -36,28 +36,110 @@ function createVoteObject(address, proposalId, support, v, r, s) {
     v,
     r,
     s,
-  }
+  };
 
   return voteObject;
 }
 
+function createDelegateVoteObject(address, delegatee, nonce, expiry, v, r, s) {
+  const delegateVoteObject = {
+    address,
+    delegatee,
+    nonce,
+    expiry,
+    v,
+    r,
+    s,
+  };
+
+  return delegateVoteObject;
+}
+
 describe('check autotask', () => {
-  it('casts votes when given a valid signature', async () => {
-    // 'function castVoteBySig(uint proposalId, uint8 support, uint8 v, bytes32 r, bytes32 s)',
-    const validVote1 = createVoteObject("0x1", 0, 0, 8, "dsadas", "dsa")
-    const validVote2 = 
+  it('casts votes when given a valid signed message', async () => {
+    const validVote1 = createCastVoteObject(
+      '0x1111111111111111111111111111111111111111',
+      0,
+      0,
+      27,
+      '0xda4429a9e8e6b54cb101b2df002039f2879ab4ca0e8fae64134942cb81f3e581',
+      '0x3b90a37dc078a82dfc418695b1d4473661aa4d24dd874ac68678894ff44a6b27',
+    );
+    const validVote2 = createCastVoteObject(
+      '0x2222222222222222222222222222222222222222',
+      0,
+      0,
+      27,
+      '0x2e04bc22ee95e9f1d77b8634a8f0bf68e73dc8dab0b531d7cce777878b2f00b0',
+      '0xd73d470d9b221117d96c88f26a57d89ae8470d986e8bac4398d8315f4cca1460',
+    );
+    const batchedTxs = [validVote1, validVote2];
+    const autotaskEvent = createAutotaskEvent(batchedTxs);
+    await handler(autotaskEvent);
+
+    expect(mockContract.castVoteBySig).toBeCalledTimes(2);
+    expect(mockContract.delegateBySig).toBeCalledTimes(0);
+    mockContract.castVoteBySig.mockClear();
+    mockContract.delegateBySig.mockClear();
   });
 
-  it('does not cast vote when signature is invalid', async () => {
+  it('delegates votes when given a valid signed message', async () => {
+    const validDelegateVote1 = createDelegateVoteObject(
+      '0x1111111111111111111111111111111111111111',
+      '0x2222222222222222222222222222222222222222',
+      1,
+      100,
+      27,
+      '0xda4429a9e8e6b54cb101b2df002039f2879ab4ca0e8fae64134942cb81f3e581',
+      '0x3b90a37dc078a82dfc418695b1d4473661aa4d24dd874ac68678894ff44a6b27',
+    );
+    const validDelegateVote2 = createDelegateVoteObject(
+      '0x2222222222222222222222222222222222222222',
+      '0x3333333333333333333333333333333333333333',
+      1,
+      100,
+      27,
+      '0x2e04bc22ee95e9f1d77b8634a8f0bf68e73dc8dab0b531d7cce777878b2f00b0',
+      '0xd73d470d9b221117d96c88f26a57d89ae8470d986e8bac4398d8315f4cca1460',
+    );
 
+    const batchedTxs = [validDelegateVote1, validDelegateVote2];
+    const autotaskEvent = createAutotaskEvent(batchedTxs);
+    await handler(autotaskEvent);
+
+    expect(mockContract.castVoteBySig).toBeCalledTimes(0);
+    expect(mockContract.delegateBySig).toBeCalledTimes(2);
+    mockContract.castVoteBySig.mockClear();
+    mockContract.delegateBySig.mockClear();
   });
 
-  it('delegates votes when given a valid signature', async () => {
-    // 'function delegateBySig(address delegatee, uint nonce, uint expiry, uint8 v, bytes32 r, bytes32 s)',
+  it('throws error if POST request information is not a delegate vote or a cast vote action', async () => {
+    // missing a support or delegatee field
+    // in practice, this should not occur, as the api endpoint makes this a required field
+    const invalidVoteObject = {
+      address: '0x1111111111111111111111111111111111111111',
+      proposalId: 0,
+      v: 27,
+      r: '0xda4429a9e8e6b54cb101b2df002039f2879ab4ca0e8fae64134942cb81f3e581',
+      s: '0x3b90a37dc078a82dfc418695b1d4473661aa4d24dd874ac68678894ff44a6b27',
+    };
 
-  });
+    // this vote should still go through, even if the first one fails
+    const validVote1 = createCastVoteObject(
+      '0x1111111111111111111111111111111111111111',
+      0,
+      0,
+      27,
+      '0xda4429a9e8e6b54cb101b2df002039f2879ab4ca0e8fae64134942cb81f3e581',
+      '0x3b90a37dc078a82dfc418695b1d4473661aa4d24dd874ac68678894ff44a6b27',
+    );
+    const batchedTxs = [invalidVoteObject, validVote1];
+    const autotaskEvent = createAutotaskEvent(batchedTxs);
+    await handler(autotaskEvent);
 
-  it('does not delegate votes signature is invalid', async () => {
-
+    expect(mockContract.castVoteBySig).toBeCalledTimes(1);
+    expect(mockContract.delegateBySig).toBeCalledTimes(0);
+    mockContract.castVoteBySig.mockClear();
+    mockContract.delegateBySig.mockClear();
   });
 });
