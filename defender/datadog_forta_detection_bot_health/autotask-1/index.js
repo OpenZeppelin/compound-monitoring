f491aceb44797865445c606a46ae55bfdb3423f0
Nomad-AE <nomad@arbitraryexecution.com>
2022-12-08 17:01:35 -0800

    AE-2182 AED-741 added DD forta detection bot

--- defender/datadog_forta_detection_bot_health/autotask-1/index.js
+++ defender/datadog_forta_detection_bot_health/autotask-1/index.js
@@ -1,3 +1,6 @@
+const stackName = 'datadog_alerts_heat_map';
+const datadogSecretName = `${stackName}_datadogApiKey`;
+
 /* eslint-disable import/no-unresolved,import/no-extraneous-dependencies */
 const axios = require('axios');
 const { KeyValueStoreClient } = require('defender-kvstore-client');
@@ -211,9 +214,10 @@ exports.handler = async function (autotaskEvent) {
     throw new Error('secrets undefined');
   }
 
-  const { DatadogApiKey: datadogApiKey } = secrets;
+  // ensure that there is a datadogApiKey secret
+  const datadogApiKey = secrets[datadogSecretName];
   if (datadogApiKey === undefined) {
-    throw new Error('Datadog API key undefined');
+    throw new Error('datadogApiKey undefined');
   }
 
   let firstRun = false;
