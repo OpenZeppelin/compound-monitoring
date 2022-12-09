89bd469299c57442f82d726aeced1beaec911801
Nomad-AE <nomad@arbitraryexecution.com>
2022-12-09 14:57:10 -0800

    AE-2182 AED-741 added forta low liquidity

--- defender/forta_low_liquidity/autotask-1/index.js
+++ defender/forta_low_liquidity/autotask-1/index.js
@@ -1,5 +1,5 @@
-// Set the name of the Secret set in Autotask
-const discordSecretName = 'SecurityAlertsDiscordUrl';
+const stackName = 'forta_low_liquidity';
+const discordSecretName = `${stackName}_discordWebhook`;
 
 const axios = require('axios');
 
