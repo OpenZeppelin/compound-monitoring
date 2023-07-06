2cb2f7e411e6368f66d6f2b5063308ea56e83ef7
Morgan Weaver <morganjweaver@gmail.com>
2023-07-06 14:25:49 +0100

    recreate branch

--- defender/forta_large_delegations/autotask-1/index.js
+++ defender/forta_large_delegations/autotask-1/index.js
@@ -1,127 +1,47 @@
-const stackName = 'forta_large_delegations';
-const discordSecretName = `${stackName}_discordWebhook`;
-
-const axios = require('axios');
-const axiosRetry = require('axios-retry');
-
-function condition(error) {
-  const result = axiosRetry.isNetworkOrIdempotentRequestError(error);
-  const rateLimit = (error.response.status === 429);
-  return result || rateLimit;
-}
-
-axiosRetry(axios, {
-  retries: 3,
-  retryDelay: axiosRetry.exponentialDelay,
-  retryCondition: condition,
-});
-
-async function postToDiscord(url, message) {
-  const method = 'post';
-  const headers = {
-    'Content-Type': 'application/json',
-  };
-  const data = { content: message };
-
-  const response = await axios({
-    url,
-    method,
-    headers,
-    data,
-  });
-  return response;
+function getUrl(scopedSecrets, source) {
+  if (!scopedSecrets || !source) return;
+  const { block, transactionHash } = source;
+  if (!block || !transactionHash) return;
+  const { chainId } = block;
+  if (!chainId) return;
+  // eslint-disable-next-line dot-notation
+  let mapping = scopedSecrets['NetworkBlockexplorerMapping'];
+  if (!mapping) return;
+  try {
+    mapping = JSON.parse(mapping);
+  } catch (e) { return; }
+  const path = mapping?.[chainId];
+  if (!path) return;
+  // eslint-disable-next-line consistent-return
+  return `${path}${transactionHash}`;
 }
 
 // eslint-disable-next-line func-names
-exports.handler = async function (autotaskEvent) {
-  // ensure that the autotaskEvent Object exists
-  if (autotaskEvent === undefined) {
-    throw new Error('autotaskEvent undefined');
-  }
-
-  const { secrets } = autotaskEvent;
-  if (secrets === undefined) {
-    throw new Error('secrets undefined');
-  }
-
-  // ensure that there is a DiscordUrl secret
-  const discordUrl = secrets[discordSecretName];
-  if (discordUrl === undefined) {
-    throw new Error('discordUrl undefined');
-  }
-
-  // Ref: https://developer.mozilla.org/en-US/docs/Web/API/URL
-  function isValidUrl(string) {
-    let url;
-    try {
-      url = new URL(string);
-    } catch (_) {
-      return false;
+exports.handler = async function (payload) {
+  const matches = [];
+  payload.request.body.events?.forEach((event) => {
+    const { source, metadata, hash } = event.alert;
+    const transactionLink = getUrl(payload.secrets, source) ?? `https://explorer.forta.network/alert/${hash}`;    
+    if (metadata === undefined) {
+      throw new Error('metadata undefined');
     }
-    return url.href;
-  }
 
-  if (isValidUrl(discordUrl) === false) {
-    throw new Error('discordUrl is not a valid URL');
-  }
+    // Start of usual modifications to the autotask script
+    // extract the metadata
+    const {
+      delegateAddress,
+      levelName,
+    } = metadata;
+    if (delegateAddress === undefined) {
+      throw new Error('delegateAddress undefined');
+    }
+    const delegateFormatted = delegateAddress.slice(0, 6);
 
-  // ensure that the request key exists within the autotaskEvent Object
-  const { request } = autotaskEvent;
-  if (request === undefined) {
-    console.error('request undefined, Autotask must be triggered by a sentinel');
-    return 'request undefined, Autotask must be triggered by a sentinel';
-  }
-
-  // ensure that the body key exists within the request Object
-  const { body } = request;
-  if (body === undefined) {
-    throw new Error('body undefined');
-  }
-
-  // ensure that the alert key exists within the body Object
-  const { alert } = body;
-  if (alert === undefined) {
-    throw new Error('alert undefined');
-  }
-
-  // ensure that the alert key exists within the body Object
-  const { source } = body;
-  if (source === undefined) {
-    throw new Error('source undefined');
-  }
-
-  // extract the metadata from the alert Object
-  const { metadata } = alert;
-  if (metadata === undefined) {
-    throw new Error('metadata undefined');
-  }
-
-  // extract the hashes from the source Object
-  const {
-    transactionHash,
-  } = source;
-
-  // Start of usual modifications to the autotask script
-  // extract the metadata
-  const {
-    delegateAddress,
-    levelName,
-  } = metadata;
-  if (delegateAddress === undefined) {
-    throw new Error('delegateAddress undefined');
-  }
-
-  const delegateFormatted = delegateAddress.slice(0, 6);
-
-  // // construct the Etherscan transaction link
-  const etherscanLink = `[TX](<https://etherscan.io/tx/${transactionHash}>)`;
-
-  const message = `${etherscanLink} 💸 **${delegateFormatted}** has been delegated  enough **COMP** tokens to pass min threshold for the governance event: **${levelName}**`;
-
-  // create promises for posting messages to Discord webhook
-  // with Log Forwarding enabled, this console.log will forward the text string to Dune Analytics
-  console.log(message);
-  await postToDiscord(discordUrl, message);
-
-  return {};
+    const message = `${transactionLink} 💸 **${delegateFormatted}** has been delegated  enough **COMP** tokens to pass min threshold for the governance event: **${levelName}**`;
+    matches.push({
+      hash: event.hash,
+      metadata: { message, transactionLink },
+    });
+  });
+  return { matches };
 };
